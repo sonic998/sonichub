1644f2b94b0e4338da5eadac8857e171d80cd3fc
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-26 23:39:35 +0100

    Update SonicLoader.lua

--- SonicLoader.lua
+++ SonicLoader.lua
@@ -7,10 +7,25 @@ SonicLoaderUI.ResetOnSpawn = false
 SonicLoaderUI.Parent = game:GetService("CoreGui")
 
 main.Name = "main"
-main.Parent = SonicUI
+main.Parent = SonicLoaderUI
 main.BackgroundColor3 = Color3.fromRGB(108, 19, 164)
 main.BorderColor3 = Color3.fromRGB(0, 0, 0)
 main.BorderSizePixel = 0
 main.Position = UDim2.new(0.304659486, 0, 0.294623643, 0)
 main.Size = UDim2.new(0, 473, 0, 288)
 main.Active = true
+
+title.Name = "title"
+title.Parent = main
+title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+title.BackgroundTransparency = 1.000
+title.BorderColor3 = Color3.fromRGB(0, 0, 0)
+title.BorderSizePixel = 0
+title.Position = UDim2.new(0.253493965, 0, -0.00537643442, 0)
+title.Size = UDim2.new(0, 232, 0, 40)
+title.Font = Enum.Font.SourceSansBold
+title.Text = "Sonic Loader"
+title.TextColor3 = Color3.fromRGB(255, 255, 255)
+title.TextScaled = true
+title.TextSize = 14.000
+title.TextWrapped = true
