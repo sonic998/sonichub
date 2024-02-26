8dcb3c4c967be5448724bb8f6b2a1c317c3cb03b
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-26 23:36:55 +0100

    Update SonicLoader.lua

--- SonicLoader.lua
+++ SonicLoader.lua
@@ -1,2 +1,7 @@
 local SonicLoaderUI = Instance.new("ScreenGui")
 local main = Instance.new("Frame")
+local title = Instance.new("TextLabel")
+
+SonicLoaderUI.Name = "SonicLoaderUI"
+SonicLoaderUI.ResetOnSpawn = false
+SonicLoaderUI.Parent = game:GetService("CoreGui")
