4f660259d1cf7e463d0b8de1c2536b9d54e4c998
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-26 21:53:19 +0100

    Add files via upload

---
+++ functions/1122519450/titansimulator.lua
@@ -0,0 +1,47 @@
+function speed()
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+local old = mt.__newindex
+
+    mt.__newindex = newcclosure(function(a, b, c)
+    if tostring(a) == "Humanoid" and tostring(b) == "WalkSpeed" then
+        return old(a, b, 1000)
+    end
+    return old(a,b,c)
+    end)
+     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1000
+end
+
+function autoswing()
+game.Players.LocalPlayer.Character:WaitForChild("Demon Sword"):Activate()
+end
+
+function autopunch()
+game.Players.LocalPlayer.Character:WaitForChild("Punch"):Activate()
+end
+
+function autorebirth()
+game.ReplicatedStorage.RemoteEventContainer.Rebirth:FireServer()
+end
+
+function equippunch()
+game.Players.LocalPlayer.Character:WaitForChild("Humanoid"):EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Punch"))
+end
+
+function equipsword()
+game.Players.LocalPlayer.Character:WaitForChild("Humanoid"):EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Demon Sword"))
+end
+
+function jump()
+game:GetService("UserInputService").jumpRequest:Connect(function()
+     game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass"Humanoid":ChangeState("Jumping")
+end)
+end
+
+function rejoin()
+game:GetService("TeleportService"):Teleport(game.PlaceId)
+end
+
+function infyield()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end
