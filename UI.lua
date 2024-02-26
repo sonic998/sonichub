8213de6a434cce0d760d681fc451bfff3a771a4d
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-26 22:12:45 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -57,6 +57,7 @@ main.BorderColor3 = Color3.fromRGB(0, 0, 0)
 main.BorderSizePixel = 0
 main.Position = UDim2.new(0.304659486, 0, 0.294623643, 0)
 main.Size = UDim2.new(0, 473, 0, 288)
+main.Active = true
 
 Instance.new("UICorner", main).CornerRadius = UDim.new(0, 9)
 
