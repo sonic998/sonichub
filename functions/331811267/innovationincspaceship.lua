0eb515b460033bc1966cbebe0675d8cf157b3648
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-26 21:52:01 +0100

    Add files via upload

---
+++ functions/331811267/innovationincspaceship.lua
@@ -0,0 +1,27 @@
+function speed()
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+local old = mt.__newindex
+
+    mt.__newindex = newcclosure(function(a, b, c)
+    if tostring(a) == "Humanoid" and tostring(b) == "WalkSpeed" then
+        return old(a, b, 100)
+    end
+    return old(a,b,c)
+    end)
+     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+end
+
+function jump()
+game:GetService("UserInputService").jumpRequest:Connect(function()
+     game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass"Humanoid":ChangeState("Jumping")
+end)
+end
+
+function rejoin()
+game:GetService("TeleportService"):Teleport(game.PlaceId)
+end
+
+function infyield()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end
