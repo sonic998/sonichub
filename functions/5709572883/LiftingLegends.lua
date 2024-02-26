e767c0dcf5acce26f26be546a769f0a5bc938030
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-26 21:55:45 +0100

    Add files via upload

---
+++ functions/5709572883/LiftingLegends.lua
@@ -0,0 +1,60 @@
+function autolift()
+local args = {
+    [1] = "Click"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Port1"):FireServer(unpack(args))
+end
+
+function autorebirth()
+local args = {
+    [1] = "Purchase",
+    [2] = "Rank"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Port1"):FireServer(unpack(args))
+end
+
+function autoascension()
+  local args = {
+    [1] = "Purchase",
+    [2] = "Ascension"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Port1"):FireServer(unpack(args))
+end
+
+function autobuy()
+local args = {
+    [1] = "PurchaseAll",
+    [2] = "Strength"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Port1"):FireServer(unpack(args))
+  local args = {
+    [1] = "PurchaseAll",
+    [2] = "Endurance"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Port1"):FireServer(unpack(args))
+end
+
+function autosell()
+local args = {
+    [1] = "SellEnergy"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Port1"):FireServer(unpack(args))
+end
+
+function rejoin()
+game:GetService("TeleportService"):Teleport(game.PlaceId)
+end
+
+function infyield()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end
+
+function antiafk()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind"))()
+end
