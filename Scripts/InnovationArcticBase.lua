41c63626bdf680d784bd6b6db77a9613c111594a
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-27 14:18:01 +0100

    Update InnovationArcticBase.lua

--- Scripts/InnovationArcticBase.lua
+++ Scripts/InnovationArcticBase.lua
@@ -1,1 +1,29 @@
+local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
+local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/1033860623/InnovationArcticBase.lua"))()
 
+local win = lib:CreateWindow("SonicHub - Innovation Arctic Base")
+local localplayer = win:Page("LocalPlayer")
+local teleport = win:Page("Teleport")
+local misc = win:Page("Misc")
+
+teleport:Button("Outside")
+
+localplayer:Button("WalkSpeed", function()
+speed()
+end)
+
+localplayer:Button("Inf Jump", function()
+jump()
+end)
+
+misc:Button("Rejoin", function()
+rejoin()
+end)
+
+misc:Button("Inf Yield", function()
+infyield()
+end)
+  
+misc:Button("Anti Afk", function()
+AntiAfk()
+end)
