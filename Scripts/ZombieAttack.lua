37ac98dd5ac5cd5ae41df451426cdc8268a1a9b5
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-27 08:45:36 +0100

    Update ZombieAttack.lua

--- Scripts/ZombieAttack.lua
+++ Scripts/ZombieAttack.lua
@@ -1,2 +1,15 @@
 local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
 local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/1240123653/ZombieAttack.lua"))()
+
+local win = lib:CreateWindow("SonicHub - Lift Legends Simulator")
+local autofarm = win:Page("AutoFarm")
+local localplayer = win:Page("LocalPlayer")
+local misc = win:Page("Misc")
+
+autofarm:Toggle("Autokill")
+
+localplayer:Button("WalkSpeed")
+localplayer:Button("Inf Jump")
+
+misc:Button("Rejoin")
+misc:Button("Anti Afk")
