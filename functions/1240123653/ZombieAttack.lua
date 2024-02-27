89f9f50aaf0de0905031d932bb14d19ea25af794
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-27 11:24:18 +0100

    Update ZombieAttack.lua

--- functions/1240123653/ZombieAttack.lua
+++ functions/1240123653/ZombieAttack.lua
@@ -36,7 +36,8 @@ bb:ClickButton2(Vector2.new())
 end)
 end
 
-function autokill()
+function autokill(toggle)
+_G.farm = toggle
 local groundDistance = 8
 local Player = game:GetService("Players").LocalPlayer
 local function getNearest()
@@ -61,17 +62,9 @@ end
 end
 return nearest
 end
-_G.farm2 = true
-Player.Chatted:Connect(function(m)
-if(m==";autofarm false")then
-_G.farm2 = false
-elseif(m==";autofarm true")then
-_G.farm2 = true
-end
-end)
 _G.globalTarget = nil
 game:GetService("RunService").RenderStepped:Connect(function()
-if(_G.farm2==true)then
+if(_G.farm==true)then
 local target = getNearest()
 if(target~=nil)then
 game:GetService("Workspace").CurrentCamera.CFrame = CFrame.new(game:GetService("Workspace").CurrentCamera.CFrame.p, target.Head.Position)
@@ -87,7 +80,7 @@ game.Players.LocalPlayer.Character.Torso.Velocity = Vector3.new(0,0,0)
 end
 end)
 while wait() do
-if(_G.farm2==true and _G.globalTarget~=nil and _G.globalTarget:FindFirstChild("Head") and Player.Character:FindFirstChildOfClass("Tool"))then
+if(_G.farm==true and _G.globalTarget~=nil and _G.globalTarget:FindFirstChild("Head") and Player.Character:FindFirstChildOfClass("Tool"))then
 local target = _G.globalTarget
 game.ReplicatedStorage.Gun:FireServer({["Normal"] = Vector3.new(0, 0, 0), ["Direction"] = target.Head.Position, ["Name"] = Player.Character:FindFirstChildOfClass("Tool").Name, ["Hit"] = target.Head, ["Origin"] = target.Head.Position, ["Pos"] = target.Head.Position,})
 wait()
