17e0dbb7efde0fa2bd3f9b07c2611fb3171b7abc
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-27 08:12:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function Join()
+{
+    window.open("https://discord.com/invite/qRXWqR4d7y");
+}
+
+function Github()
+{
+    window.open("https://github.com/sonic998");
+}
+
+function SonicMovie()
+{
+    window.open("https://drive.google.com/drive/folders/1_hG-4Shhcv-lf5rDhpfDJ21_rLnlbjX6?usp=sharing");
+}
+
+function SonicPrime()
+{
+    window.open("https://mega.nz/folder/yPARzRCC#q2DcHE79ZATyPTDiXDiw3A");
+}
+
+function SonicX()
+{
+    window.open("https://drive.google.com/drive/folders/1V3l7_dESUzUB2aWch5W7Ab26u7hczCgs?usp=sharing");
+}
+
+function SonicForcesSongs()
+{
+    window.open("https://drive.google.com/file/d/1fzxYOqdU3GePu95u-KcvLryQBDOynXJB/view?usp=sharing");
+}
+
+function SonicFrontiersSongs()
+{
+    window.open("https://drive.google.com/file/d/1ur0REVuoalQ-ciBgiLD3nJOj2Ev9g7-j/view?usp=sharing");
+}
+
+function SonicHub()
+{
+	window.open("https://sonic998.github.io/sonichub");
+}
+
+function copyscript()
+{
+	 navigator.clipboard.writeText("loadstring(game:HttpGet('https://sonic998.github.io/sonichub/SonicHub.lua'))()")
+}
