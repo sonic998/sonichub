71a39623e977f5f16c019a2e861b1fe5a9f7de67
sonic <156506053+sonic998@users.noreply.github.com>
2024-02-27 08:44:08 +0100

    Update ZombieAttack.lua

--- Scripts/ZombieAttack.lua
+++ Scripts/ZombieAttack.lua
@@ -1,1 +1,2 @@
-
+local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
+local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/1240123653/ZombieAttack.lua"))()
