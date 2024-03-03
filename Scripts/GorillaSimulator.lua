4a012d64c1b9c0bc323a7d5702704adef0a6ff6a
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-03 11:54:44 +0100

    Update GorillaSimulator.lua

--- Scripts/GorillaSimulator.lua
+++ Scripts/GorillaSimulator.lua
@@ -1,1 +1,34 @@
+local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
+local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/1378712700/GorillaSimulator.lua"))()
 
+local win = lib:CreateWindow("SonicHub - Gorilla Simulator 2")
+local autofarm = win:Page("AutoFarm")
+local localplayer = win:Page("LocalPlayer")
+local misc = win:Page("Misc")
+
+autofarm:Toggle("Auto Eat Banana", function(v)
+getgenv().autoeat = v
+while getgenv().autoeat do wait(0.1)
+autoeat()
+end
+end)
+
+localplayer:Button("WalkSpeed", function()
+walkspeed()
+end)
+
+localplayer:Button("Inf Jump", function()
+infjump()
+end)
+
+misc:Button("Rejoin", function()
+  rejoin()
+end)
+
+misc:Button("Inf Yield", function()
+infyield()
+end)
+
+misc:Button("Anti Afk", function()
+antiafk()
+end)
