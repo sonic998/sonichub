ee66f07527bb248583636641000c49d9893cfa8e
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-03 11:57:01 +0100

    Update GorillaSimulator.lua

--- functions/1378712700/GorillaSimulator.lua
+++ functions/1378712700/GorillaSimulator.lua
@@ -1,1 +1,43 @@
+function speed()
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+local old = mt.__newindex
 
+    mt.__newindex = newcclosure(function(a, b, c)
+    if tostring(a) == "Humanoid" and tostring(b) == "WalkSpeed" then
+        return old(a, b, 100)
+    end
+    return old(a,b,c)
+    end)
+     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+end
+
+function autoeat()
+for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+if v.Name == "Banana" then
+v:Activate()
+end
+end
+end
+
+function jump()
+game:GetService("UserInputService").jumpRequest:Connect(function()
+     game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass"Humanoid":ChangeState("Jumping")
+end)
+end
+
+function rejoin()
+game:GetService("TeleportService"):Teleport(game.PlaceId)
+end
+
+function infyield()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end
+    
+function AntiAfk()
+local bb=game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+bb:CaptureController()
+bb:ClickButton2(Vector2.new())
+end)
+end
