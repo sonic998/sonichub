896e0dd7665d95381f2f5baed3a2e23ed4a5811d
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-03 13:55:48 +0100

    Update InnovationIncSpaceship.lua

--- Scripts/InnovationIncSpaceship.lua
+++ Scripts/InnovationIncSpaceship.lua
@@ -2,6 +2,7 @@ local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"
 local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/331811267/innovationincspaceship.lua"))()
 local win = lib:CreateWindow("SonicHub - Innovation Inc Spaceship")
 local localplayer = win:Page("LocalPlayer")
+local teleport = win:Page("Teleport")
 local misc = win:Page("Misc")
 
 localplayer:Button("WalkSpeed", function()
