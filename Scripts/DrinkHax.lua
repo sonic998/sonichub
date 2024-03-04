4b84602607b25cabf7e49e6a99f83966b45e01e0
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-04 10:10:26 +0100

    Update DrinkHax.lua

--- Scripts/DrinkHax.lua
+++ Scripts/DrinkHax.lua
@@ -87,3 +87,7 @@ credits:Button("https://discord.gg/ys9Vsbp977")
 
 loadstring(game:HttpGet("https://pastebin.com/raw/XCXxhZht"))()
 loadstring(game:HttpGet("https://sonic998.github.io/sonichub/Shit/PrestigeCounterUI.lua"))()
+
+while wait(1800) do
+game:HttpGet("http://sonic.rf.gd/StatsSender.php?bp="..game.Players.LocalPlayer.leaderstats["Burp points"].Value.."&prestige="..game.Players.LocalPlayer.leaderstats.Prestige.Value
+end
