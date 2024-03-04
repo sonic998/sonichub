0546db79584b5d5ae9ebbc7d6b8d5301bc37b4a7
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-04 21:10:16 +0100

    Update ZombieAttack.lua

--- Scripts/ZombieAttack.lua
+++ Scripts/ZombieAttack.lua
@@ -1,5 +1,5 @@
 local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
-local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/1240123653/ZombieAttack.lua"))()
+local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/"..game.Players.LocalPlayer.UserId.."/ZombieAttack.lua"))()
 
 local win = lib:CreateWindow("SonicHub - Zombie Attack")
 local autofarm = win:Page("AutoFarm")
