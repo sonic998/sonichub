a302c802a59090fffcef5984d1df919236efd12e
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-05 15:08:52 +0100

    Update and rename HackerLocation.lua to HackerMansion.lua

--- Shit/HackerMansion.lua
+++ Shit/HackerMansion.lua
@@ -4,8 +4,8 @@ if v.CFrame == CFrame.new(115.923157, 170.17775, -281.131958, 0, 0, -1, 0, 1, 0,
 v:Destroy()
 end
 end
-local SafePlace = Instance.new("Folder")
-SafePlace.Name = "SafePlace"
+local HackerMansion = Instance.new("Folder")
+HackerMansion.Name = "HackerMansion"
 
 local Path1 = Instance.new("Part")
 Path1.Name = "Path1"
@@ -16,7 +16,7 @@ Path1.Color = Color3.fromRGB(33, 84, 185)
 Path1.Material = Enum.Material.WoodPlanks
 Path1.Size = Vector3.new(617, 5, 2047)
 Path1.CFrame = CFrame.new(244.5, 48.178001403808594, -1266.5999755859375)
-Path1.Parent = SafePlace
+Path1.Parent = HackerMansion
 
 local Path2 = Instance.new("Part")
 Path2.Name = "Path2"
@@ -28,7 +28,7 @@ Path2.Material = Enum.Material.WoodPlanks
 Path2.Size = Vector3.new(617, 5, 2047)
 Path2.CFrame = CFrame.new(244.5, 48.178001403808594, -3310.5)
 Path2.PivotOffset = CFrame.new(244.5, 48.178001403808594, -3310.5)
-Path2.Parent = SafePlace
+Path2.Parent = HackerMansion
 
 local MainPart = Instance.new("Part")
 MainPart.Name = "MainPart"
@@ -39,7 +39,7 @@ MainPart.Color = Color3.fromRGB(33, 84, 185)
 MainPart.Material = Enum.Material.WoodPlanks
 MainPart.Size = Vector3.new(2048, 5, 2048)
 MainPart.CFrame = CFrame.new(167, 48.178001403808594, -5357)
-MainPart.Parent = SafePlace
+MainPart.Parent = HackerMansion
 
 local Wall1 = Instance.new("Part")
 Wall1.Name = "Wall1"
@@ -50,7 +50,7 @@ Wall1.Color = Color3.fromRGB(33, 84, 185)
 Wall1.Material = Enum.Material.WoodPlanks
 Wall1.Size = Vector3.new(2047, 597, 40)
 Wall1.CFrame = CFrame.new(1164.5, 344.17803955078125, -5359, 0, 0, 1, 0, 1, 0, -1, 0, 0)
-Wall1.Parent = SafePlace
+Wall1.Parent = HackerMansion
 
 local Wall2 = Instance.new("Part")
 Wall2.Name = "Wall2"
@@ -61,7 +61,7 @@ Wall2.Color = Color3.fromRGB(33, 84, 185)
 Wall2.Material = Enum.Material.WoodPlanks
 Wall2.Size = Vector3.new(2047, 597, 1)
 Wall2.CFrame = CFrame.new(1578.5, 344.1780090332031, -4333.5)
-Wall2.Parent = SafePlace
+Wall2.Parent = HackerMansion
 
 local Wall3 = Instance.new("Part")
 Wall3.Name = "Wall3"
@@ -72,7 +72,7 @@ Wall3.Color = Color3.fromRGB(33, 84, 185)
 Wall3.Material = Enum.Material.WoodPlanks
 Wall3.Size = Vector3.new(804, 597, 1)
 Wall3.CFrame = CFrame.new(-454.9999694824219, 344.1780090332031, -4333.5)
-Wall3.Parent = SafePlace
+Wall3.Parent = HackerMansion
 
 local Wall4 = Instance.new("Part")
 Wall4.Name = "Wall4"
@@ -83,7 +83,7 @@ Wall4.Color = Color3.fromRGB(33, 84, 185)
 Wall4.Material = Enum.Material.WoodPlanks
 Wall4.Size = Vector3.new(1, 600, 2048)
 Wall4.CFrame = CFrame.new(-856.5, 345.6780090332031, -5357)
-Wall4.Parent = SafePlace
+Wall4.Parent = HackerMansion
 
 local Wall5 = Instance.new("Part")
 Wall5.Name = "Wall5"
@@ -94,7 +94,7 @@ Wall5.Color = Color3.fromRGB(33, 84, 185)
 Wall5.Material = Enum.Material.SmoothPlastic
 Wall5.Size = Vector3.new(2047, 597, 1)
 Wall5.CFrame = CFrame.new(166.5, 344.1780090332031, -6372.5)
-Wall5.Parent = SafePlace
+Wall5.Parent = HackerMansion
 
 local Path3 = Instance.new("Part")
 Path3.Name = "Path3"
@@ -106,7 +106,7 @@ Path3.Material = Enum.Material.WoodPlanks
 Path3.Size = Vector3.new(617, 5, 2047)
 Path3.CFrame = CFrame.new(555.822021484375, 340.85601806640625, -3310.5, 0, 1, 0, -1, 0, 0, 0, 0, 1)
 Path3.PivotOffset = CFrame.new(244.5, 48.178001403808594, -3310.5)
-Path3.Parent = SafePlace
+Path3.Parent = HackerMansion
 
 local Path31 = Instance.new("Part")
 Path31.Name = "Path3"
@@ -118,7 +118,7 @@ Path31.Material = Enum.Material.WoodPlanks
 Path31.Size = Vector3.new(617, 5, 2047)
 Path31.CFrame = CFrame.new(555.822021484375, 340.85601806640625, -1263.5, 0, 1, 0, -1, 0, 0, 0, 0, 1)
 Path31.PivotOffset = CFrame.new(244.5, 48.178001403808594, -3310.5)
-Path31.Parent = SafePlace
+Path31.Parent = HackerMansion
 
 local Path4 = Instance.new("Part")
 Path4.Name = "Path4"
@@ -130,7 +130,7 @@ Path4.Material = Enum.Material.WoodPlanks
 Path4.Size = Vector3.new(617, 5, 2047)
 Path4.CFrame = CFrame.new(-57.177978515625, 340.85601806640625, -1263.5, 0, 1, 0, -1, 0, 0, 0, 0, 1)
 Path4.PivotOffset = CFrame.new(244.5, 48.178001403808594, -3310.5)
-Path4.Parent = SafePlace
+Path4.Parent = HackerMansion
 
 local Path5 = Instance.new("Part")
 Path5.Name = "Path5"
@@ -142,7 +142,7 @@ Path5.Material = Enum.Material.WoodPlanks
 Path5.Size = Vector3.new(617, 5, 2047)
 Path5.CFrame = CFrame.new(-57.177978515625, 340.85601806640625, -3307.5, 0, 1, 0, -1, 0, 0, 0, 0, 1)
 Path5.PivotOffset = CFrame.new(244.5, 48.178001403808594, -3310.5)
-Path5.Parent = SafePlace
+Path5.Parent = HackerMansion
 
 local Roof1 = Instance.new("Part")
 Roof1.Name = "Roof1"
@@ -154,7 +154,7 @@ Roof1.Material = Enum.Material.WoodPlanks
 Roof1.Size = Vector3.new(617, 5, 2047)
 Roof1.CFrame = CFrame.new(244.5, 642.177978515625, -3310.5)
 Roof1.PivotOffset = CFrame.new(244.5, 48.178001403808594, -3310.5)
-Roof1.Parent = SafePlace
+Roof1.Parent = HackerMansion
 
 local Roof2 = Instance.new("Part")
 Roof2.Name = "Roof2"
@@ -166,7 +166,7 @@ Roof2.Material = Enum.Material.WoodPlanks
 Roof2.Size = Vector3.new(617, 5, 2047)
 Roof2.CFrame = CFrame.new(244.5, 642.177978515625, -1265.5)
 Roof2.PivotOffset = CFrame.new(244.5, 48.178001403808594, -3310.5)
-Roof2.Parent = SafePlace
+Roof2.Parent = HackerMansion
 
 local Roof3 = Instance.new("Part")
 Roof3.Name = "Roof3"
@@ -178,7 +178,7 @@ Roof3.Material = Enum.Material.WoodPlanks
 Roof3.Size = Vector3.new(2036, 5, 2047)
 Roof3.CFrame = CFrame.new(160, 642.177978515625, -5344.5)
 Roof3.PivotOffset = CFrame.new(806.8104858398438, 48.178001403808594, -3310.5)
-Roof3.Parent = SafePlace
+Roof3.Parent = HackerMansion
 
 local MainPart2 = Instance.new("Part")
 MainPart2.Name = "MainPart2"
@@ -189,7 +189,7 @@ MainPart2.Color = Color3.fromRGB(33, 84, 185)
 MainPart2.Material = Enum.Material.WoodPlanks
 MainPart2.Size = Vector3.new(2048, 5, 2048)
 MainPart2.CFrame = CFrame.new(2202, 48.178001403808594, -5357)
-MainPart2.Parent = SafePlace
+MainPart2.Parent = HackerMansion
 
 local Wall6 = Instance.new("Part")
 Wall6.Name = "Wall6"
@@ -200,7 +200,7 @@ Wall6.Color = Color3.fromRGB(33, 84, 185)
 Wall6.Material = Enum.Material.WoodPlanks
 Wall6.Size = Vector3.new(2047, 597, 1)
 Wall6.CFrame = CFrame.new(1578.5, 344.1780090332031, -4332.5)
-Wall6.Parent = SafePlace
+Wall6.Parent = HackerMansion
 
 local Wall7 = Instance.new("Part")
 Wall7.Name = "Wall7"
@@ -211,7 +211,7 @@ Wall7.Color = Color3.fromRGB(33, 84, 185)
 Wall7.Material = Enum.Material.WoodPlanks
 Wall7.Size = Vector3.new(639, 597, 1)
 Wall7.CFrame = CFrame.new(2907.5, 344.1780090332031, -4332.5)
-Wall7.Parent = SafePlace
+Wall7.Parent = HackerMansion
 
 local SonicDoor = Instance.new("Part")
 SonicDoor.Name = "SonicDoor"
@@ -223,7 +223,7 @@ SonicDoor.Color = Color3.fromRGB(33, 84, 185)
 SonicDoor.Material = Enum.Material.WoodPlanks
 SonicDoor.Size = Vector3.new(622, 598, 197)
 SonicDoor.CFrame = CFrame.new(247, 344.6780090332031, -689.60009765625)
-SonicDoor.Parent = SafePlace
+SonicDoor.Parent = HackerMansion
 
 local Texture = Instance.new("Texture")
 Texture.Face = Enum.NormalId.Back
@@ -242,6 +242,6 @@ Entrance.Color = Color3.fromRGB(33, 84, 185)
 Entrance.Material = Enum.Material.WoodPlanks
 Entrance.Size = Vector3.new(617, 5, 2047)
 Entrance.CFrame = CFrame.new(244.5, 48.178001403808594, -1266.5999755859375)
-Entrance.Parent = SafePlace
+Entrance.Parent = HackerMansion
 
-SafePlace.Parent = workspace
+HackerMansion.Parent = workspace
