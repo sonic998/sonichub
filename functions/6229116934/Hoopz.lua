2655cc81904e5563a335a7b3fda46c20db93bb88
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-24 23:00:40 +0100

    Update Hoopz.lua

--- functions/6229116934/Hoopz.lua
+++ functions/6229116934/Hoopz.lua
@@ -1,1 +1,37 @@
+function speed()
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+local old = mt.__newindex
 
+    mt.__newindex = newcclosure(function(a, b, c)
+    if tostring(a) == "Humanoid" and tostring(b) == "WalkSpeed" then
+        return old(a, b, 100)
+    end
+    return old(a,b,c)
+    end)
+  while wait() do
+     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
+  end
+end
+
+function jump()
+  game:GetService("UserInputService").jumpRequest:Connect(function()
+     game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass"Humanoid":ChangeState("Jumping")
+end)
+end
+
+function rejoin()
+  game:GetService("TeleportService"):Teleport(game.PlaceId)
+end
+
+function infyield()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end
+
+function AntiAfk()
+local bb=game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+bb:CaptureController()
+bb:ClickButton2(Vector2.new())
+end)
+end
