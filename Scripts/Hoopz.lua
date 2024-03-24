35d60357aea8fc3eeab15918f920f94e37cc843e
sonic <156506053+sonic998@users.noreply.github.com>
2024-03-24 23:04:34 +0100

    Create Hoopz.lua

---
+++ Scripts/Hoopz.lua
@@ -0,0 +1,27 @@
+local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
+local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/"..game.PlaceId.."/Hoopz.lua"))()
+
+local win = lib:CreateWindow("SonicHub - Hoopz")
+local autofarm = win:Page("AutoFarm")
+local localplayer = win:Page("LocalPlayer")
+local misc = win:Page("Misc")
+
+localplayer:Button("WalkSpeed", function()
+speed()
+end)
+
+localplayer:Button("Inf Jump", function()
+jump()
+end)
+
+misc:Button("Rejoin", function()
+rejoin()
+end)
+
+misc:Button("Inf Yield", function()
+infyield()
+end)
+
+misc:Button("Anti Afk", function()
+AntiAfk()
+end)
