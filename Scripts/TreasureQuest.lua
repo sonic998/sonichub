0b9f3b8e3e78c8570695e452ad57a75d38e0090f
sonic <156506053+sonic998@users.noreply.github.com>
2024-06-28 16:12:48 +0200

    Create TreasureQuest.lua

---
+++ Scripts/TreasureQuest.lua
@@ -0,0 +1,32 @@
+local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
+
+local win = lib:CreateWindow("SonicHub - Treasure Quest")
+local autofarm = win:Page("AutoFarm")
+local localplayer = win:Page("LocalPlayer")
+local misc = win:Page("Misc")
+
+autofarm:Button("Autofarm", function(v)
+while wait(0.1) do
+autofarm()
+end
+end)
+
+localplayer:Button("WalkSpeed", function()
+  speed()
+end)
+
+localplayer:Button("Inf Jump", function()
+  infjump()
+end)
+
+misc:Button("Rejoin", function()
+  rejoin()
+end)
+
+misc:Button("Inf Yield", function()
+infyield()
+end)
+
+misc:Button("Anti Afk", function()
+antiafk()
+end)
