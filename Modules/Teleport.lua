30e3c0136f70117ba71d656e3f71791ecd04a9a6
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-24 21:39:58 +0200

    Update Teleport.lua

--- Modules/Teleport.lua
+++ Modules/Teleport.lua
@@ -1,11 +1,14 @@
+function ybsScPhjSmpAG(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
 local shit = {}
 
 function shit:Tp(Position)
-game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = Position
+game:GetService(ybsScPhjSmpAG({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:WaitForChild(ybsScPhjSmpAG({7560,12285,11445,10185,11550,11655,11025,10500,8610,11655,11655,12180,8400,10185,11970,12180})).CFrame = Position
 end
 
 function shit:Tween(Time, Position)
-game:GetService("TweenService"):Create(game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart"), Time, {CFrame = Position}):Play()
+game:GetService(ybsScPhjSmpAG({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game:GetService(ybsScPhjSmpAG({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:WaitForChild(ybsScPhjSmpAG({7560,12285,11445,10185,11550,11655,11025,10500,8610,11655,11655,12180,8400,10185,11970,12180})), Time, {CFrame = Position}):Play()
 end
 
-return shit
+return shit    
