49c8bbb7a086c6433f28d0c780ef21326a0bf26b
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-24 21:40:30 +0200

    Update loadstring.lua

--- Modules/loadstring.lua
+++ Modules/loadstring.lua
@@ -1,7 +1,10 @@
+function UdxQhgCjFtURFJHqRrRUewVdtjwGCbiDgIoGAbqjnvxpgTWC(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
 local loadstring = {}
 
 function loadstring:Make(url)
-setclipboard("loadstring(game:HttpGet('"..url.."'))()")
+setclipboard(UdxQhgCjFtURFJHqRrRUewVdtjwGCbiDgIoGAbqjnvxpgTWC({11340,11655,10185,10500,12075,12180,11970,11025,11550,10815,4200,10815,10185,11445,10605,6090,7560,12180,12180,11760,7455,10605,12180,4200})UdxQhgCjFtURFJHqRrRUewVdtjwGCbiDgIoGAbqjnvxpgTWC({4830,4830,12285,11970,11340,4830,4830})UdxQhgCjFtURFJHqRrRUewVdtjwGCbiDgIoGAbqjnvxpgTWC({4305,4305,4200,4305}))
 end
 
-return loadstring
+return loadstring    
