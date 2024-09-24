46366ce524cadf067d7a64296ea438c92a9dbdff
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-24 21:55:29 +0200

    Update UrMomGui.lua

--- Shit/UrMomGui.lua
+++ Shit/UrMomGui.lua
@@ -1,10 +1,13 @@
-local UrMomUi = Instance.new("ScreenGui")
-UrMomUi.Name = "UrMomUi"
+function KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
+local UrMomUi = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8715,10395,11970,10605,10605,11550,7455,12285,11025}))
+UrMomUi.Name = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8925,11970,8085,11655,11445,8925,11025})
 UrMomUi.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 UrMomUi.ResetOnSpawn = false
 
-local UMMain = Instance.new("Frame")
-UMMain.Name = "UMMain"
+local UMMain = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({7350,11970,10185,11445,10605}))
+UMMain.Name = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8925,8085,8085,10185,11025,11550})
 UMMain.Size = UDim2.new(0.1569187, 0, 0.2215744, 0)
 UMMain.BorderColor3 = Color3.fromRGB(27, 42, 53)
 UMMain.Position = UDim2.new(0.1954351, 0, 0.6836735, 0)
@@ -14,11 +17,11 @@ UMMain.Parent = UrMomUi
 UMMain.Active = true
 UMMain.Draggable = true
 
-local UICorner = Instance.new("UICorner")
+local UICorner = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8925,7665,7035,11655,11970,11550,10605,11970}))
 UICorner.Parent = UMMain
 
-local Line1 = Instance.new("TextLabel")
-Line1.Name = "Line1"
+local Line1 = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8820,10605,12600,12180,7980,10185,10290,10605,11340}))
+Line1.Name = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({7980,11025,11550,10605,5145})
 Line1.TextTransparency = 1
 Line1.AutomaticSize = Enum.AutomaticSize.X
 Line1.Size = UDim2.new(0.9545455, 0, 0.1973684, 0)
@@ -29,15 +32,15 @@ Line1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Line1.FontSize = Enum.FontSize.Size14
 Line1.TextSize = 14
 Line1.TextColor3 = Color3.fromRGB(255, 91, 208)
-Line1.Text = "You earn about:"
+Line1.Text = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({9345,11655,12285,3360,10605,10185,11970,11550,3360,10185,10290,11655,12285,12180,6090})
 Line1.TextWrap = true
 Line1.Font = Enum.Font.FredokaOne
 Line1.TextWrapped = true
 Line1.TextScaled = true
 Line1.Parent = UMMain
 
-local Line2 = Instance.new("TextLabel")
-Line2.Name = "Line2"
+local Line2 = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8820,10605,12600,12180,7980,10185,10290,10605,11340}))
+Line2.Name = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({7980,11025,11550,10605,5250})
 Line2.TextTransparency = 1
 Line2.AutomaticSize = Enum.AutomaticSize.X
 Line2.Size = UDim2.new(0.9545455, 0, 0.1973684, 0)
@@ -48,15 +51,15 @@ Line2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Line2.FontSize = Enum.FontSize.Size14
 Line2.TextSize = 10
 Line2.TextColor3 = Color3.fromRGB(255, 215, 0)
-Line2.Text = "Burp Points per second"
+Line2.Text = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({6930,12285,11970,11760,3360,8400,11655,11025,11550,12180,12075,3360,11760,10605,11970,3360,12075,10605,10395,11655,11550,10500})
 Line2.TextWrap = true
 Line2.Font = Enum.Font.FredokaOne
 Line2.TextWrapped = true
 Line2.TextScaled = true
 Line2.Parent = UMMain
 
-local Title = Instance.new("TextLabel")
-Title.Name = "Title"
+local Title = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8820,10605,12600,12180,7980,10185,10290,10605,11340}))
+Title.Name = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8820,11025,12180,11340,10605})
 Title.Size = UDim2.new(0.5772728, 0, 0.125, 0)
 Title.BackgroundTransparency = 1
 Title.Position = UDim2.new(0.0227273, 0, 0, 0)
@@ -65,7 +68,7 @@ Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Title.FontSize = Enum.FontSize.Size14
 Title.TextSize = 14
 Title.TextColor3 = Color3.fromRGB(255, 255, 255)
-Title.Text = "UrMom GUI"
+Title.Text = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8925,11970,8085,11655,11445,3360,7455,8925,7665})
 Title.TextWrap = true
 Title.Font = Enum.Font.FredokaOne
 Title.TextWrapped = true
@@ -73,9 +76,9 @@ Title.TextXAlignment = Enum.TextXAlignment.Left
 Title.TextScaled = true
 Title.Parent = UMMain
 
-local WhenCalibrated = Instance.new("TextLabel")
+local WhenCalibrated = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8820,10605,12600,12180,7980,10185,10290,10605,11340}))
 WhenCalibrated.TextTransparency = 1
-WhenCalibrated.Name = "WhenCalibrated"
+WhenCalibrated.Name = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({9135,10920,10605,11550,7035,10185,11340,11025,10290,11970,10185,12180,10605,10500})
 WhenCalibrated.Size = UDim2.new(0.909091, 0, 0.368421, 0)
 WhenCalibrated.BackgroundTransparency = 1
 WhenCalibrated.Position = UDim2.new(0.0454545, 0, 0.3355263, 0)
@@ -84,7 +87,7 @@ WhenCalibrated.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 WhenCalibrated.FontSize = Enum.FontSize.Size14
 WhenCalibrated.TextSize = 14
 WhenCalibrated.TextColor3 = Color3.fromRGB(8, 143, 143)
-WhenCalibrated.Text = "0"
+WhenCalibrated.Text = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({5040})
 WhenCalibrated.TextWrap = true
 WhenCalibrated.Font = Enum.Font.FredokaOne
 WhenCalibrated.TextWrapped = true
@@ -92,14 +95,14 @@ WhenCalibrated.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
 WhenCalibrated.TextScaled = true
 WhenCalibrated.Parent = UMMain
 
-local UIGradient = Instance.new("UIGradient")
+local UIGradient = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8925,7665,7455,11970,10185,10500,11025,10605,11550,12180}))
 UIGradient.Rotation = 60
 UIGradient.Color = ColorSequence.new({ColorSequenceKeypoint.new(0,Color3.fromRGB(44,46,54)),ColorSequenceKeypoint.new(0.6455907,Color3.fromRGB(228,233,255)),ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,255))})
 UIGradient.Offset = Vector2.new(0.050000000745058, -0.050000000745058)
 UIGradient.Parent = UMMain
 
-local BeforeCalibrated = Instance.new("TextLabel")
-BeforeCalibrated.Name = "BeforeCalibrated"
+local BeforeCalibrated = Instance.new(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8820,10605,12600,12180,7980,10185,10290,10605,11340}))
+BeforeCalibrated.Name = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({6930,10605,10710,11655,11970,10605,7035,10185,11340,11025,10290,11970,10185,12180,10605,10500})
 BeforeCalibrated.Size = UDim2.new(0.909091, 0, 0.3552632, 0)
 BeforeCalibrated.BackgroundTransparency = 1
 BeforeCalibrated.Position = UDim2.new(0.0454545, 0, 0.3223684, 0)
@@ -108,7 +111,7 @@ BeforeCalibrated.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 BeforeCalibrated.FontSize = Enum.FontSize.Size14
 BeforeCalibrated.TextSize = 14
 BeforeCalibrated.TextColor3 = Color3.fromRGB(255, 255, 255)
-BeforeCalibrated.Text = "Awaiting Calibration. Please wait."
+BeforeCalibrated.Text = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({6825,12495,10185,11025,12180,11025,11550,10815,3360,7035,10185,11340,11025,10290,11970,10185,12180,11025,11655,11550,4830,3360,8400,11340,10605,10185,12075,10605,3360,12495,10185,11025,12180,4830})
 BeforeCalibrated.TextWrap = true
 BeforeCalibrated.Font = Enum.Font.FredokaOne
 BeforeCalibrated.TextWrapped = true
@@ -118,7 +121,7 @@ BeforeCalibrated.Parent = UMMain
 
 UrMomUi.Parent = game.CoreGui
 
-local ts = game:GetService("TweenService")
+local ts = game:GetService(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605}))
 
 local need_tweens = {
     WhenCalibrated,
@@ -136,16 +139,16 @@ local function on_calibrated()
 	end)
 end
 
-local leaderstats = game:GetService("Players").LocalPlayer.leaderstats
+local leaderstats = game:GetService(KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.leaderstats
 index = -1
 calibrated = false
 while true do
 	index = index + 1
-	strbefore = leaderstats["Burp points"].Value
-	sps = 'Calibrating'
+	strbefore = leaderstats[KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({6930,12285,11970,11760,3360,11760,11655,11025,11550,12180,12075})].Value
+	sps = KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({7035,10185,11340,11025,10290,11970,10185,12180,11025,11550,10815})
 	wait(1)
 	if index > 3 then
-		strafter = leaderstats["Burp points"].Value-strbefore
+		strafter = leaderstats[KBJRAetrgdeWYuZvpElAcyqJTNDFFbtlGizJojbTXgsLBsFlmCejfRcvmBbGNEVfXPMxaoUhNufvBkazONoqHmRzJKzClqGEbi({6930,12285,11970,11760,3360,11760,11655,11025,11550,12180,12075})].Value-strbefore
 		sps = tostring(strafter)
 		WhenCalibrated.Text = sps
 		if calibrated ~= true then
@@ -153,4 +156,4 @@ while true do
 		    calibrated = true
 		end
 	end
-end
+end    
