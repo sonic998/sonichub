9507bf212c3f6d417b22fea90bd2efce086c1f75
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-24 21:42:31 +0200

    Update InnovationArcticBase.lua

--- Scripts/InnovationArcticBase.lua
+++ Scripts/InnovationArcticBase.lua
@@ -1,29 +1,32 @@
-local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
-local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/"..game.PlaceId.."/InnovationArcticBase.lua"))()
+function aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
 
-local win = lib:CreateWindow("SonicHub - Innovation Arctic Base")
-local localplayer = win:Page("LocalPlayer")
-local teleport = win:Page("Teleport")
-local misc = win:Page("Misc")
 
-teleport:Button("Outside")
+local lib = loadstring(game:HttpGet(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8925,7665,4830,11340,12285,10185})))()
+local functions = loadstring(game:HttpGet(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,10710,12285,11550,10395,12180,11025,11655,11550,12075,4935})..game.PlaceId..aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({4935,7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,6825,11970,10395,12180,11025,10395,6930,10185,12075,10605,4830,11340,12285,10185})))()
 
-localplayer:Button("WalkSpeed", function()
+local win = lib:CreateWindow(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({8715,11655,11550,11025,10395,7560,12285,10290,3360,4725,3360,7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,3360,6825,11970,10395,12180,11025,10395,3360,6930,10185,12075,10605}))
+local localplayer = win:Page(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({7980,11655,10395,10185,11340,8400,11340,10185,12705,10605,11970}))
+local teleport = win:Page(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({8820,10605,11340,10605,11760,11655,11970,12180}))
+local misc = win:Page(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({8085,11025,12075,10395}))
+
+teleport:Button(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({8295,12285,12180,12075,11025,10500,10605}))
+
+localplayer:Button(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({9135,10185,11340,11235,8715,11760,10605,10605,10500}), function()
 speed()
 end)
 
-localplayer:Button("Inf Jump", function()
+localplayer:Button(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({7665,11550,10710,3360,7770,12285,11445,11760}), function()
 jump()
 end)
 
-misc:Button("Rejoin", function()
+misc:Button(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({8610,10605,11130,11655,11025,11550}), function()
 rejoin()
 end)
 
-misc:Button("Inf Yield", function()
+misc:Button(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({7665,11550,10710,3360,9345,11025,10605,11340,10500}), function()
 infyield()
 end)
   
-misc:Button("Anti Afk", function()
+misc:Button(aWfzFfDHYHkZGmAGMhGxyPNhHynxaCsaCQxApcrJyrbByjyvmeMlxZTn({6825,11550,12180,11025,3360,6825,10710,11235}), function()
 AntiAfk()
-end)
+end)    
