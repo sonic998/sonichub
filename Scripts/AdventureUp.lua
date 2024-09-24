916dd77335fc387a2f351eab9c6013e7e6fe96fe
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-24 21:40:59 +0200

    Update AdventureUp.lua

--- Scripts/AdventureUp.lua
+++ Scripts/AdventureUp.lua
@@ -1,27 +1,30 @@
-local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
-local functions = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/functions/3194820651/AdventureUp.lua"))()
+function SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
 
-local win = lib:CreateWindow("SonicHub - Adventure Up")
-local autofarm = win:Page("AutoFarm")
-local localplayer = win:Page("LocalPlayer")
-local misc = win:Page("Misc")
 
-autofarm:Button("Auto Time Trail", function(v)
+local lib = loadstring(game:HttpGet(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8925,7665,4830,11340,12285,10185})))()
+local functions = loadstring(game:HttpGet(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,10710,12285,11550,10395,12180,11025,11655,11550,12075,4935,5355,5145,5985,5460,5880,5250,5040,5670,5565,5145,4935,6825,10500,12390,10605,11550,12180,12285,11970,10605,8925,11760,4830,11340,12285,10185})))()
+
+local win = lib:CreateWindow(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({8715,11655,11550,11025,10395,7560,12285,10290,3360,4725,3360,6825,10500,12390,10605,11550,12180,12285,11970,10605,3360,8925,11760}))
+local autofarm = win:Page(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({6825,12285,12180,11655,7350,10185,11970,11445}))
+local localplayer = win:Page(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({7980,11655,10395,10185,11340,8400,11340,10185,12705,10605,11970}))
+local misc = win:Page(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({8085,11025,12075,10395}))
+
+autofarm:Button(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({6825,12285,12180,11655,3360,8820,11025,11445,10605,3360,8820,11970,10185,11025,11340}), function(v)
 timetrail()
 end)
 
-localplayer:Button("WalkSpeed", function()
+localplayer:Button(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({9135,10185,11340,11235,8715,11760,10605,10605,10500}), function()
   speed()
 end)
 
-misc:Button("Rejoin", function()
+misc:Button(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({8610,10605,11130,11655,11025,11550}), function()
   rejoin()
 end)
 
-misc:Button("Inf Yield", function()
+misc:Button(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({7665,11550,10710,3360,9345,11025,10605,11340,10500}), function()
 infyield()
 end)
 
-misc:Button("Anti Afk", function()
+misc:Button(SjJMMPclAfQpFmBrqFgBqToxucawZpYgzDVCxgfqkbMegaPmadJwdHwRF({6825,11550,12180,11025,3360,6825,10710,11235}), function()
 antiafk()
-end)
+end)    
