8e630454f52b0f2552c69ea7125a84d3378193f0
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-27 09:32:23 +0200

    Update GamesMenu.lua

--- Shit/GamesMenu.lua
+++ Shit/GamesMenu.lua
@@ -1,129 +1,132 @@
-local gamesTab = win:Page("Games")
-gamesTab:Label("Current Game")
-local CurrentLabel =  gamesTab:Label("")
+function yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
+local gamesTab = win:Page(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7455,10185,11445,10605,12075}))
+gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7035,12285,11970,11970,10605,11550,12180,3360,7455,10185,11445,10605}))
+local CurrentLabel =  gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({}))
 
 if game.PlaceId == 1122519450 then
-CurrentLabel:Update("Titan Simulator")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,11025,12180,10185,11550,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}))
 end
 if game.PlaceId == 5709572883 then
-CurrentLabel:Update("Lufting Legends")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7980,12285,10710,12180,11025,11550,10815,3360,7980,10605,10815,10605,11550,10500,12075}))
 end
 if game.PlaceId == 331811267 then
-CurrentLabel:Update("Innovation Inc Space ship")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,3360,7665,11550,10395,3360,8715,11760,10185,10395,10605,3360,12075,10920,11025,11760}))
 end
 if game.PlaceId == 1033860623 then
-CurrentLabel:Update("Innovation ArcticbBase")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,3360,6825,11970,10395,12180,11025,10395,10290,6930,10185,12075,10605}))
 end
 if game.PlaceId == 1747207098 then
-CurrentLabel:Update("Drinkhax")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7140,11970,11025,11550,11235,10920,10185,12600}))
 end
 if game.PlaceId == 1240123653 then
-CurrentLabel:Update("Zombie Attack")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({9450,11655,11445,10290,11025,10605,3360,6825,12180,12180,10185,10395,11235}))
 end
 if game.PlaceId == 1502601752 then
-CurrentLabel:Update("Rabbit Simulator")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8610,10185,10290,10290,11025,12180,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}))
 end
 if game.PlaceId == 1378712700 then
-CurrentLabel:Update("Gorilla Simulator")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7455,11655,11970,11025,11340,11340,10185,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}))
 end
 if game.PlaceId == 13928941253 then
-CurrentLabel:Update("Ultra Lifting Simulator")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8925,11340,12180,11970,10185,3360,7980,11025,10710,12180,11025,11550,10815,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}))
 end
 if game.PlaceId == 2960777560 then
-CurrentLabel:Update("Treasure Quest")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,11970,10605,10185,12075,12285,11970,10605,3360,8505,12285,10605,12075,12180}))
 end
 if game.PlaceId == 3194820651 then
-CurrentLabel:Update("Adventure Up")
+CurrentLabel:Update(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({6825,10500,12390,10605,11550,12180,12285,11970,10605,3360,8925,11760}))
 end
 
 --Grinding Games
 local SelectGrinding
-gamesTab:Drop("Grinding/lifting games", {"Titan Simulator", "Lift Legends", "Burping Simulator(drinkhax)", "Rabbit Simulator", "Gorilla Simulator", "Ultra Lifting Simulator"}, function(v)
+gamesTab:Drop(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7455,11970,11025,11550,10500,11025,11550,10815,4935,11340,11025,10710,12180,11025,11550,10815,3360,10815,10185,11445,10605,12075}), {yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,11025,12180,10185,11550,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}), yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7980,11025,10710,12180,3360,7980,10605,10815,10605,11550,10500,12075}), yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({6930,12285,11970,11760,11025,11550,10815,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970,4200,10500,11970,11025,11550,11235,10920,10185,12600,4305}), yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8610,10185,10290,10290,11025,12180,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}), yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7455,11655,11970,11025,11340,11340,10185,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}), yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8925,11340,12180,11970,10185,3360,7980,11025,10710,12180,11025,11550,10815,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970})}, function(v)
 SelectGrinding:Update(v)
-if v == "Titan Simulator" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,11025,12180,10185,11550,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}) then
 _G.GrindingOption = 1122519450
 end
-if v == "Lift Legends" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7980,11025,10710,12180,3360,7980,10605,10815,10605,11550,10500,12075}) then
 _G.GrindingOption = 5709572883
 end
-if v == "Burping Simulator(drinkhax)" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({6930,12285,11970,11760,11025,11550,10815,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970,4200,10500,11970,11025,11550,11235,10920,10185,12600,4305}) then
 _G.GrindingOption = 1747207098
 end
-if v == "Rabbit Simulator" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8610,10185,10290,10290,11025,12180,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}) then
 _G.GrindingOption = 1502601752
 end
-if v == "Gorilla Simulator" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7455,11655,11970,11025,11340,11340,10185,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}) then
 _G.GrindingOption = 1378712700
 end
-if v == "Ultra Lifting Simulator" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8925,11340,12180,11970,10185,3360,7980,11025,10710,12180,11025,11550,10815,3360,8715,11025,11445,12285,11340,10185,12180,11655,11970}) then
 _G.GrindingOption = 13928941253
 end
 end)
 
-gamesTab:Label("Selected game")
-SelectGrinding = gamesTab:Label("")
-local TeleportService = game:GetService("TeleportService")
-gamesTab:Button("Join game", function()
+gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8715,10605,11340,10605,10395,12180,10605,10500,3360,10815,10185,11445,10605}))
+SelectGrinding = gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({}))
+local TeleportService = game:GetService(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605}))
+gamesTab:Button(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7770,11655,11025,11550,3360,10815,10185,11445,10605}), function()
 TeleportService:Teleport(_G.GrindingOption)
 end)
-gamesTab:Label("-------------")
+gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725}))
 
 --Innovation inc
 
 local SelectInmovationInc
-gamesTab:Drop("Innovation Inc games", {"Innovation Inc Space ship", "Innovation Arctic Base"}, function(v)
+gamesTab:Drop(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,3360,7665,11550,10395,3360,10815,10185,11445,10605,12075}), {yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,3360,7665,11550,10395,3360,8715,11760,10185,10395,10605,3360,12075,10920,11025,11760}), yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,3360,6825,11970,10395,12180,11025,10395,3360,6930,10185,12075,10605})}, function(v)
 SelectInmovationInc:Update(v)
-if v == "Innovation Inc Space ship" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,3360,7665,11550,10395,3360,8715,11760,10185,10395,10605,3360,12075,10920,11025,11760}) then
 _G.InmovationInc = 331811267
 end
-if v == "Innovation Arctic Base" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7665,11550,11550,11655,12390,10185,12180,11025,11655,11550,3360,6825,11970,10395,12180,11025,10395,3360,6930,10185,12075,10605}) then
 _G.InmovationInc = 1033860623
 end
 end)
 
-gamesTab:Label("Selected game")
-SelectInmovationInc = gamesTab:Label("")
-local TeleportService = game:GetService("TeleportService")
-gamesTab:Button("Join game", function()
+gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8715,10605,11340,10605,10395,12180,10605,10500,3360,10815,10185,11445,10605}))
+SelectInmovationInc = gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({}))
+local TeleportService = game:GetService(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605}))
+gamesTab:Button(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7770,11655,11025,11550,3360,10815,10185,11445,10605}), function()
 TeleportService:Teleport(_G.InmovationInc)
 end)
-gamesTab:Label("-------------")
+gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725}))
 
 --Zombie Shooter
 
 local SelectZombieShooter
-gamesTab:Drop("Zombie Shooter games", {"Zombie Attack"}, function(v)
+gamesTab:Drop(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({9450,11655,11445,10290,11025,10605,3360,8715,10920,11655,11655,12180,10605,11970,3360,10815,10185,11445,10605,12075}), {yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({9450,11655,11445,10290,11025,10605,3360,6825,12180,12180,10185,10395,11235})}, function(v)
 SelectZombieShooter:Update(v)
-if v == "Zombie Attack" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({9450,11655,11445,10290,11025,10605,3360,6825,12180,12180,10185,10395,11235}) then
 _G.ZombieShooter = 1240123653
 end
 end)
 
-gamesTab:Label("Selected game")
-SelectZombieShooter = gamesTab:Label("")
-local TeleportService = game:GetService("TeleportService")
-gamesTab:Button("Join game", function()
+gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8715,10605,11340,10605,10395,12180,10605,10500,3360,10815,10185,11445,10605}))
+SelectZombieShooter = gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({}))
+local TeleportService = game:GetService(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605}))
+gamesTab:Button(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7770,11655,11025,11550,3360,10815,10185,11445,10605}), function()
 TeleportService:Teleport(_G.ZombieShooter)
 end)
 
-gamesTab:Label("-------------")
+gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725,4725}))
 
 --Dungeon RPG
 
 local SelectDungeonRPG
-gamesTab:Drop("Dungeon RPG games", {"Treasure Quest", "Adventure Up"}, function(v)
+gamesTab:Drop(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7140,12285,11550,10815,10605,11655,11550,3360,8610,8400,7455,3360,10815,10185,11445,10605,12075}), {yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,11970,10605,10185,12075,12285,11970,10605,3360,8505,12285,10605,12075,12180}), yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({6825,10500,12390,10605,11550,12180,12285,11970,10605,3360,8925,11760})}, function(v)
 SelectDungeonRPG:Update(v)
-if v == "Treasure Quest" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,11970,10605,10185,12075,12285,11970,10605,3360,8505,12285,10605,12075,12180}) then
 _G.DungeonRPG = 2960777560
 end
-if v == "Adventure Up" then
+if v == yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({6825,10500,12390,10605,11550,12180,12285,11970,10605,3360,8925,11760}) then
 _G.DungeonRPG = 3194820651
 end
 end)
 
-gamesTab:Label("Selected game")
-SelectDungeonRPG = gamesTab:Label("")
-local TeleportService = game:GetService("TeleportService")
-gamesTab:Button("Join game", function()
+gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8715,10605,11340,10605,10395,12180,10605,10500,3360,10815,10185,11445,10605}))
+SelectDungeonRPG = gamesTab:Label(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({}))
+local TeleportService = game:GetService(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605}))
+gamesTab:Button(yeOSaCypIvlLCjbQCSQdEyFtbxWtBMSFHJaAYEVxXoBDjMYqHGFmahRj({7770,11655,11025,11550,3360,10815,10185,11445,10605}), function()
 TeleportService:Teleport(_G.DungeonRPG)
-end)
+end)    
