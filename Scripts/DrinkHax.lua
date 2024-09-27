a67ac95bf7acd280dcab0be6f47e32b51d8a6600
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-27 16:15:14 +0200

    Update DrinkHax.lua

--- Scripts/DrinkHax.lua
+++ Scripts/DrinkHax.lua
@@ -1,67 +1,72 @@
-function WnhpSanAWiNLwind(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+function KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
 
 
-if workspace:FindFirstChild(WnhpSanAWiNLwind({7560,10185,10395,11235,10605,11970,8085,10185,11550,12075,11025,11655,11550})) then
-  workspace:FindFirstChild(WnhpSanAWiNLwind({7560,10185,10395,11235,10605,11970,8085,10185,11550,12075,11025,11655,11550})):Destroy()
+if workspace:FindFirstChild(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7560,10185,10395,11235,10605,11970,8085,10185,11550,12075,11025,11655,11550})) then
+  workspace:FindFirstChild(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7560,10185,10395,11235,10605,11970,8085,10185,11550,12075,11025,11655,11550})):Destroy()
 end
 
-local lib = loadstring(game:HttpGet(WnhpSanAWiNLwind({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8925,7665,4830,11340,12285,10185})))()
-loadstring(game:HttpGet(WnhpSanAWiNLwind({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,10710,12285,11550,10395,12180,11025,11655,11550,12075,4935})..game.PlaceId..WnhpSanAWiNLwind({4935,7140,11970,11025,11550,11235,7560,10185,12600,4830,11340,12285,10185})))()
+local lib = loadstring(game:HttpGet(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8925,7665,4830,11340,12285,10185})))()
+loadstring(game:HttpGet(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,10710,12285,11550,10395,12180,11025,11655,11550,12075,4935})..game.PlaceId..KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({4935,7140,11970,11025,11550,11235,7560,10185,12600,4830,11340,12285,10185})))()
 
-_G.win = lib:CreateWindow(WnhpSanAWiNLwind({8715,11655,11550,11025,10395,7560,12285,10290,3360,4725,3360,7140,11970,11025,11550,11235,10920,10185,12600}))
-local autofarm = _G.win:Page(WnhpSanAWiNLwind({6825,12285,12180,11655,7350,10185,11970,11445}))
-local localplayer = _G.win:Page(WnhpSanAWiNLwind({7980,11655,10395,10185,11340,8400,11340,10185,12705,10605,11970}))
-local WorldChanges = _G.win:Page(WnhpSanAWiNLwind({9135,11655,11970,11340,10500,3360,7035,10920,10185,11550,10815,10605,12075}))
-local teleport = _G.win:Page(WnhpSanAWiNLwind({8820,10605,11340,10605,11760,11655,11970,12180}))
-local misc = _G.win:Page(WnhpSanAWiNLwind({8085,11025,12075,10395}))
-loadstring(game:HttpGet(WnhpSanAWiNLwind({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,7455,10185,11445,10605,12075,8085,10605,11550,12285,4830,11340,12285,10185})))()
+_G.win = lib:CreateWindow(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8715,11655,11550,11025,10395,7560,12285,10290,3360,4725,3360,7140,11970,11025,11550,11235,10920,10185,12600}))
+local autofarm = _G.win:Page(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({6825,12285,12180,11655,7350,10185,11970,11445}))
+local localplayer = _G.win:Page(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7980,11655,10395,10185,11340,8400,11340,10185,12705,10605,11970}))
+local WorldChanges = _G.win:Page(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({9135,11655,11970,11340,10500,3360,7035,10920,10185,11550,10815,10605,12075}))
+local teleport = _G.win:Page(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8820,10605,11340,10605,11760,11655,11970,12180}))
+local misc = _G.win:Page(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8085,11025,12075,10395}))
+loadstring(game:HttpGet(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,7455,10185,11445,10605,12075,8085,10605,11550,12285,4830,11340,12285,10185})))()
 
-WorldChanges:Toggle(WnhpSanAWiNLwind({8190,11655,10395,11340,11025,11760,3360,6930,11655,11970,10500,10605,11970,3360,9135,10185,11340,11340,12075}), function(bool)
+WorldChanges:Toggle(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8190,11655,10395,11340,11025,11760,3360,6930,11655,11970,10500,10605,11970,3360,9135,10185,11340,11340,12075}), function(bool)
+if bool == true then
+_G.bool = false
+elseif bool == false then
+_G.bool = true
+end
 for i,v in pairs(workspace.BorderWalls:GetChildren()) do
-v.CanCollide = bool
+v.CanCollide = _G.bool
 end
 end)
 
-WorldChanges:Toggle(WnhpSanAWiNLwind({9135,10185,11340,11235,3360,11655,11550,3360,12495,10185,12180,10605,11970}), function(bool)
+WorldChanges:Toggle(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({9135,10185,11340,11235,3360,11655,11550,3360,12495,10185,12180,10605,11970}), function(bool)
 for i,v in pairs(workspace:GetChildren()) do
-if v:IsA(WnhpSanAWiNLwind({8400,10185,11970,12180})) then
-if v.Color = Color3.fromRGB(9, 137, 207) then
+if v:IsA(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8400,10185,11970,12180})) then
+if v.Color == Color3.fromRGB(9, 137, 207) then
 v.CanCollide = bool
 end
 end
 end
 end)
 
-teleport:Button(WnhpSanAWiNLwind({7035,11340,11655,12285,10500,3360,8715,10185,10710,10605,3360,8400,11340,10185,10395,10605}), function()
-game:GetService(WnhpSanAWiNLwind({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(291, 809, -913)}):Play()
+teleport:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7035,11340,11655,12285,10500,3360,8715,10185,10710,10605,3360,8400,11340,10185,10395,10605}), function()
+game:GetService(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(291, 809, -913)}):Play()
 end)
-teleport:Button(WnhpSanAWiNLwind({8715,10185,10710,10605,3360,9450,11655,11550,10605}), function()
-game:GetService(WnhpSanAWiNLwind({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(-53, 48, -37)}):Play()
+teleport:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8715,10185,10710,10605,3360,9450,11655,11550,10605}), function()
+game:GetService(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(-53, 48, -37)}):Play()
 end)
-teleport:Button(WnhpSanAWiNLwind({7140,11025,12075,10395,11655,3360,7665,12075,11340,10185,11550,10500}), function()
-game:GetService(WnhpSanAWiNLwind({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(53, 48, 613)}):Play()
+teleport:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7140,11025,12075,10395,11655,3360,7665,12075,11340,10185,11550,10500}), function()
+game:GetService(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(53, 48, 613)}):Play()
 end)
-teleport:Button(WnhpSanAWiNLwind({8715,10605,10395,11655,11550,10500,3360,7665,12075,11340,10185,11550,10500}), function()
-game:GetService(WnhpSanAWiNLwind({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(-1180, 48, 0)}):Play()
+teleport:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8715,10605,10395,11655,11550,10500,3360,7665,12075,11340,10185,11550,10500}), function()
+game:GetService(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(-1180, 48, 0)}):Play()
 end)
-teleport:Button(WnhpSanAWiNLwind({8715,11235,12705,3360,7665,12075,11340,10185,11550,10500}), function()
-game:GetService(WnhpSanAWiNLwind({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(2031, 1456, -1055)}):Play()
+teleport:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8715,11235,12705,3360,7665,12075,11340,10185,11550,10500}), function()
+game:GetService(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(2031, 1456, -1055)}):Play()
 end)
-teleport:Button(WnhpSanAWiNLwind({7035,11340,11655,12285,10500,3360,8295,11550,10605}), function()
-game:GetService(WnhpSanAWiNLwind({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(291, 558, 646)}):Play()
+teleport:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7035,11340,11655,12285,10500,3360,8295,11550,10605}), function()
+game:GetService(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(291, 558, 646)}):Play()
 end)
-teleport:Button(WnhpSanAWiNLwind({7035,11340,11655,12285,10500,3360,8820,12495,11655}), function()
-game:GetService(WnhpSanAWiNLwind({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(-1214, 570, -283)}):Play()
+teleport:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7035,11340,11655,12285,10500,3360,8820,12495,11655}), function()
+game:GetService(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8820,12495,10605,10605,11550,8715,10605,11970,12390,11025,10395,10605})):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.2), {CFrame = CFrame.new(-1214, 570, -283)}):Play()
 end)
 
-autofarm:Toggle(WnhpSanAWiNLwind({6825,12285,12180,11655,3360,7140,11970,11025,11550,11235}), function(v)
+autofarm:Toggle(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({6825,12285,12180,11655,3360,7140,11970,11025,11550,11235}), function(v)
 getgenv().autodrink = v
 while getgenv().autodrink do wait(2.34)
 autodrink()
 end
 end)
 
-autofarm:Toggle(WnhpSanAWiNLwind({6825,12285,12180,11655,3360,8400,11970,10605,12075,12180,11025,10815,10605}), function(v)
+autofarm:Toggle(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({6825,12285,12180,11655,3360,8400,11970,10605,12075,12180,11025,10815,10605}), function(v)
 getgenv().autoprestige = v
 while getgenv().autoprestige do wait(1)
 autoprestige()
@@ -70,46 +75,46 @@ end)
 
 equipdrink()
 
-autofarm:Toggle(WnhpSanAWiNLwind({6825,12285,12180,11655,3360,7245,11865,12285,11025,11760,3360,7140,11970,11025,11550,11235}), function(v)
+autofarm:Toggle(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({6825,12285,12180,11655,3360,7245,11865,12285,11025,11760,3360,7140,11970,11025,11550,11235}), function(v)
 getgenv().equipdrink = v
 while getgenv().equipdrink do wait(0.5)
 AutoEquipDrink()
 end
 end)
 
-autofarm:Toggle(WnhpSanAWiNLwind({6825,12285,12180,11655,3360,7035,11655,11340,11340,10605,10395,12180,3360,7455,10605,11445,12075}), function(v)
+autofarm:Toggle(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({6825,12285,12180,11655,3360,7035,11655,11340,11340,10605,10395,12180,3360,7455,10605,11445,12075}), function(v)
 getgenv().collectgems = v
 while getgenv().collectgems do wait(0.5)
 AutoCollectGems()
 end
 end)
 
-localplayer:Button(WnhpSanAWiNLwind({9135,10185,11340,11235,8715,11760,10605,10605,10500}), function()
+localplayer:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({9135,10185,11340,11235,8715,11760,10605,10605,10500}), function()
 speed()
 end)
 
-localplayer:Button(WnhpSanAWiNLwind({7665,11550,10710,3360,7770,12285,11445,11760}), function()
+localplayer:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7665,11550,10710,3360,7770,12285,11445,11760}), function()
 jump()
 end)
 
-misc:Button(WnhpSanAWiNLwind({8610,10605,11130,11655,11025,11550}), function()
+misc:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({8610,10605,11130,11655,11025,11550}), function()
 rejoin()
 end)
 
-misc:Button(WnhpSanAWiNLwind({7665,11550,10710,3360,9345,11025,10605,11340,10500}), function()
+misc:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7665,11550,10710,3360,9345,11025,10605,11340,10500}), function()
 infyield()
 end)
-misc:Button(WnhpSanAWiNLwind({6825,11550,12180,11025,3360,7875,11025,10395,11235}), function()
+misc:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({6825,11550,12180,11025,3360,7875,11025,10395,11235}), function()
 AntiKick()
 end)
 
-misc:Button(WnhpSanAWiNLwind({6825,11550,12180,11025,3360,6825,10710,11235}), function()
+misc:Button(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({6825,11550,12180,11025,3360,6825,10710,11235}), function()
 AntiAfk()
 end)
 
-misc:Toggle(WnhpSanAWiNLwind({6930,11760,3360,7035,11655,12285,11550,12180,10605,11970,3360,9030,11025,12075,11025,10290,11340,11025,12180,12705}), function(v)
-game:GetService(WnhpSanAWiNLwind({7035,11655,11970,10605,7455,12285,11025})).GainUI.Enabled = v
+misc:Toggle(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({6930,11760,3360,7035,11655,12285,11550,12180,10605,11970,3360,9030,11025,12075,11025,10290,11340,11025,12180,12705}), function(v)
+game:GetService(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({7035,11655,11970,10605,7455,12285,11025})).GainUI.Enabled = v
 end)
     
-loadstring(game:HttpGet(WnhpSanAWiNLwind({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,6930,11760,7035,11655,12285,11550,12180,10605,11970,4830,11340,12285,10185})))()
+loadstring(game:HttpGet(KnIoslAOiSBywihbUkUdXrgcKUNqXeafeNgkQkCUsrtgamiaIGGCUSsIXtShguIujRhACQlSXTolfXekAUfjwWJxjbq({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,6930,11760,7035,11655,12285,11550,12180,10605,11970,4830,11340,12285,10185})))()
     
