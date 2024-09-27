7f4cb06882d45ae8ff50c819a3ec9ab375e8a760
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-27 15:48:05 +0200

    Update DrinkHax.lua

--- functions/1747207098/DrinkHax.lua
+++ functions/1747207098/DrinkHax.lua
@@ -1,18 +1,21 @@
-loadstring(game:HttpGet("https://sonic998.github.io/sonichub/Shit/HackerMansion.lua"))()
+function zYBgTemXmOWPgliotNCIclc(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
+loadstring(game:HttpGet(zYBgTemXmOWPgliotNCIclc({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,7560,10185,10395,11235,10605,11970,8085,10185,11550,12075,11025,11655,11550,4830,11340,12285,10185})))()
 function autodrink()
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Starter Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Second Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Third Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Fourth Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Fifth Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Sixth Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Seventh Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Eighth Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Ninth Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Atomic Drink")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Omega Burp Juice")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Thunder Fizz")
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Garlic Juice")
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({8715,12180,10185,11970,12180,10605,11970,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({8715,10605,10395,11655,11550,10500,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({8820,10920,11025,11970,10500,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({7350,11655,12285,11970,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({7350,11025,10710,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({8715,11025,12600,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({8715,10605,12390,10605,11550,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({7245,11025,10815,10920,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({8190,11025,11550,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({6825,12180,11655,11445,11025,10395,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({8295,11445,10605,10815,10185,3360,6930,12285,11970,11760,3360,7770,12285,11025,10395,10605}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({8820,10920,12285,11550,10500,10605,11970,3360,7350,11025,12810,12810}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(zYBgTemXmOWPgliotNCIclc({7455,10185,11970,11340,11025,10395,3360,7770,12285,11025,10395,10605}))
 end
 
 function AutoCollectGems()
@@ -26,7 +29,7 @@ function autoprestige()
 end
 
 function equipdrink()
-  loadstring(game:HttpGet("https://sonic998.github.io/sonichub/Shit/AutoEquipDrink.lua"))()
+  loadstring(game:HttpGet(zYBgTemXmOWPgliotNCIclc({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,6825,12285,12180,11655,7245,11865,12285,11025,11760,7140,11970,11025,11550,11235,4830,11340,12285,10185})))()
 end
 
 function speed()
@@ -35,43 +38,43 @@ setreadonly(mt, false)
 local old = mt.__newindex
 
     mt.__newindex = newcclosure(function(a, b, c)
-    if tostring(a) == "Humanoid" and tostring(b) == "WalkSpeed" then
+    if tostring(a) == zYBgTemXmOWPgliotNCIclc({7560,12285,11445,10185,11550,11655,11025,10500}) and tostring(b) == zYBgTemXmOWPgliotNCIclc({9135,10185,11340,11235,8715,11760,10605,10605,10500}) then
         return old(a, b, 1000)
     end
     return old(a,b,c)
     end)
-  game:GetService("RunService").Stepped:Connect(function()
+  game:GetService(zYBgTemXmOWPgliotNCIclc({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605})).Stepped:Connect(function()
      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1000 
   end)
 end
 
 function jump()
-  game:GetService("UserInputService").JumpRequest:Connect(function()
-     game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+  game:GetService(zYBgTemXmOWPgliotNCIclc({8925,12075,10605,11970,7665,11550,11760,12285,12180,8715,10605,11970,12390,11025,10395,10605})).JumpRequest:Connect(function()
+     game:GetService(zYBgTemXmOWPgliotNCIclc({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:FindFirstChildOfClass(zYBgTemXmOWPgliotNCIclc({7560,12285,11445,10185,11550,11655,11025,10500})):ChangeState(zYBgTemXmOWPgliotNCIclc({7770,12285,11445,11760,11025,11550,10815}))
 end)
 end
 
 function rejoin()
-  game:GetService("TeleportService"):Teleport(game.PlaceId)
+  game:GetService(zYBgTemXmOWPgliotNCIclc({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605})):Teleport(game.PlaceId)
 end
 
 function infyield()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+  loadstring(game:HttpGet(zYBgTemXmOWPgliotNCIclc({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,7245,10500,10815,10605,7665,9345,4935,11025,11550,10710,11025,11550,11025,12180,10605,12705,11025,10605,11340,10500,4935,11445,10185,12075,12180,10605,11970,4935,12075,11655,12285,11970,10395,10605})))()
 end
 
 for i,v in pairs(workspace.Clouds:GetChildren()) do
-if v.Name == "Cloud" then
+if v.Name == zYBgTemXmOWPgliotNCIclc({7035,11340,11655,12285,10500}) then
 v.CanCollide = true
 end
 end
 
 function BpInvis()
-game:GetService("CoreGui").GainUI.Enabled = v
+game:GetService(zYBgTemXmOWPgliotNCIclc({7035,11655,11970,10605,7455,12285,11025})).GainUI.Enabled = v
 end
 
 function walkonwater()
 for i,v in pairs(workspace:GetChildren()) do
-if v:IsA("Part") then
+if v:IsA(zYBgTemXmOWPgliotNCIclc({8400,10185,11970,12180})) then
 if v.Color = Color3.fromRGB(9, 137, 207) then
 v.CanCollide = v
 end
@@ -91,8 +94,8 @@ end
 end
 
 function AntiAfk()
-local bb=game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:Connect(function()
+local bb=game:GetService(zYBgTemXmOWPgliotNCIclc({9030,11025,11970,12180,12285,10185,11340,8925,12075,10605,11970}))
+game:GetService(zYBgTemXmOWPgliotNCIclc({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Idled:Connect(function()
 bb:CaptureController()
 bb:ClickButton2(Vector2.new())
 end)
@@ -105,7 +108,7 @@ local mt = getrawmetatable(game)
         setreadonly(mt, false)
         mt.__namecall = protect(function(self, ...)
             local method = getnamecallmethod()
-            if method == "Kick" then
+            if method == zYBgTemXmOWPgliotNCIclc({7875,11025,10395,11235}) then
                 wait(9e9)
                 return
             end
@@ -113,3 +116,4 @@ local mt = getrawmetatable(game)
         end)
         hookfunction(game.Players.LocalPlayer.Kick,protect(function() wait(9e9) end))
 end
+    
