a2df85ba3ecba7ca2276092ab2ddddeb5bfcccb0
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-29 16:04:41 +0200

    Update DrinkHax.lua

--- functions/1747207098/DrinkHax.lua
+++ functions/1747207098/DrinkHax.lua
@@ -1,21 +1,21 @@
-function eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+function jtfuPKdqxspEwIScglJeCr(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
 
 
-loadstring(game:HttpGet(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,7560,10185,10395,11235,10605,11970,8085,10185,11550,12075,11025,11655,11550,4830,11340,12285,10185})))()
+loadstring(game:HttpGet(jtfuPKdqxspEwIScglJeCr({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,7560,10185,10395,11235,10605,11970,8085,10185,11550,12075,11025,11655,11550,4830,11340,12285,10185})))()
 function autodrink()
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8715,12180,10185,11970,12180,10605,11970,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8715,10605,10395,11655,11550,10500,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8820,10920,11025,11970,10500,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7350,11655,12285,11970,12180,10920,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7350,11025,10710,12180,10920,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8715,11025,12600,12180,10920,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8715,10605,12390,10605,11550,12180,10920,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7245,11025,10815,10920,12180,10920,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8190,11025,11550,12180,10920,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({6825,12180,11655,11445,11025,10395,3360,7140,11970,11025,11550,11235}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8295,11445,10605,10815,10185,3360,6930,12285,11970,11760,3360,7770,12285,11025,10395,10605}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8820,10920,12285,11550,10500,10605,11970,3360,7350,11025,12810,12810}))
-  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7455,10185,11970,11340,11025,10395,3360,7770,12285,11025,10395,10605}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({8715,12180,10185,11970,12180,10605,11970,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({8715,10605,10395,11655,11550,10500,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({8820,10920,11025,11970,10500,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({7350,11655,12285,11970,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({7350,11025,10710,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({8715,11025,12600,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({8715,10605,12390,10605,11550,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({7245,11025,10815,10920,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({8190,11025,11550,12180,10920,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({6825,12180,11655,11445,11025,10395,3360,7140,11970,11025,11550,11235}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({8295,11445,10605,10815,10185,3360,6930,12285,11970,11760,3360,7770,12285,11025,10395,10605}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({8820,10920,12285,11550,10500,10605,11970,3360,7350,11025,12810,12810}))
+  game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer(jtfuPKdqxspEwIScglJeCr({7455,10185,11970,11340,11025,10395,3360,7770,12285,11025,10395,10605}))
 end
 
 function AutoCollectGems()
@@ -31,27 +31,27 @@ function autoprestige()
 end
 
 function equipdrink()
-  loadstring(game:HttpGet(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,6825,12285,12180,11655,7245,11865,12285,11025,11760,7140,11970,11025,11550,11235,4830,11340,12285,10185})))()
+  loadstring(game:HttpGet(jtfuPKdqxspEwIScglJeCr({10920,12180,12180,11760,12075,6090,4935,4935,12075,11655,11550,11025,10395,5985,5985,5880,4830,10815,11025,12180,10920,12285,10290,4830,11025,11655,4935,12075,11655,11550,11025,10395,10920,12285,10290,4935,8715,10920,11025,12180,4935,6825,12285,12180,11655,7245,11865,12285,11025,11760,7140,11970,11025,11550,11235,4830,11340,12285,10185})))()
 end
 
 function automine()
-game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character.Pickaxe.Server.Mine:FireServer()
+game:GetService(jtfuPKdqxspEwIScglJeCr({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character.Pickaxe.Server.Mine:FireServer()
 end
 
 function AutoEquipPickaxe()
-if game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Backpack.Pickaxe ~= nil then
-game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character.Humanoid:EquipTool(game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Backpack.Pickaxe)
+if game:GetService(jtfuPKdqxspEwIScglJeCr({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Backpack.Pickaxe ~= nil then
+game:GetService(jtfuPKdqxspEwIScglJeCr({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character.Humanoid:EquipTool(game:GetService(jtfuPKdqxspEwIScglJeCr({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Backpack.Pickaxe)
 end
 end
 
 function ShiftLock()
-loadstring(game:HttpGet(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({10920,12180,12180,11760,12075,6090,4935,4935,12075,10395,11970,11025,11760,12180,10290,11340,11655,12600,4830,10395,11655,11445,4935,11970,10185,12495,4935,8925,11550,11025,12390,10605,11970,12075,10185,11340,4725,8715,10395,11970,11025,11760,12180,4725,8400,10605,11970,11445,10185,11550,10605,11550,12180,4725,8715,10920,11025,10710,12180,11340,11655,10395,11235,4725,5775,5565,5145,5355})))()
+loadstring(game:HttpGet(jtfuPKdqxspEwIScglJeCr({10920,12180,12180,11760,12075,6090,4935,4935,12075,10395,11970,11025,11760,12180,10290,11340,11655,12600,4830,10395,11655,11445,4935,11970,10185,12495,4935,8925,11550,11025,12390,10605,11970,12075,10185,11340,4725,8715,10395,11970,11025,11760,12180,4725,8400,10605,11970,11445,10185,11550,10605,11550,12180,4725,8715,10920,11025,10710,12180,11340,11655,10395,11235,4725,5775,5565,5145,5355})))()
 end
 
 function WalkonWater(bool)
 getgenv().walkonwater = bool
 for i,v in pairs(workspace:GetChildren()) do
-		if v:IsA(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8400,10185,11970,12180})) then
+		if v:IsA(jtfuPKdqxspEwIScglJeCr({8400,10185,11970,12180})) then
 			if v.Color == Color3.fromRGB(9, 137, 207) then
 				v.CanCollide = getgenv().walkonwater
 			end
@@ -73,12 +73,14 @@ end
 
 function BpCounter(bool)
 getgenv().bpcounter = bool
-game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7035,11655,11970,10605,7455,12285,11025})).GainUI.Enabled = getgenv().bpcounter
+game:GetService(jtfuPKdqxspEwIScglJeCr({7035,11655,11970,10605,7455,12285,11025})).GainUI.Enabled = getgenv().bpcounter
 end
 
 function HideDrink(num)
 getgenv().hidedrink = num
-game.Players.LocalPlayer.Character:FindFirstChildWhichIsA(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8820,11655,11655,11340})).Handle.Transparency = getgenv().hidedrink
+while wait() do
+game.Players.LocalPlayer.Character:FindFirstChildWhichIsA(jtfuPKdqxspEwIScglJeCr({8820,11655,11655,11340})).Handle.Transparency = getgenv().hidedrink
+end
 end
 
 function RainbowDrink(bool)
@@ -87,23 +89,23 @@ local t = 2;
 
 	local tick = tick
 	local fromHSV = Color3.fromHSV
-	local RunService = game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605}))
+	local RunService = game:GetService(jtfuPKdqxspEwIScglJeCr({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605}))
 
-	RunService:BindToRenderStep(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8610,10185,11025,11550,10290,11655,12495}), 1000, function()
+	RunService:BindToRenderStep(jtfuPKdqxspEwIScglJeCr({8610,10185,11025,11550,10290,11655,12495}), 1000, function()
 		if getgenv().rainbow == true then
 			local hue = tick() % t / t
 			local color = fromHSV(hue, 1, 1)
-			game.Players.LocalPlayer.Character:FindFirstChildWhichIsA(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8820,11655,11655,11340})).Handle.Mesh.TextureId = eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({})
-			game.Players.LocalPlayer.Character:FindFirstChildWhichIsA(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8820,11655,11655,11340})).Handle.Color = color
+			game.Players.LocalPlayer.Character:FindFirstChildWhichIsA(jtfuPKdqxspEwIScglJeCr({8820,11655,11655,11340})).Handle.Mesh.TextureId = jtfuPKdqxspEwIScglJeCr({})
+			game.Players.LocalPlayer.Character:FindFirstChildWhichIsA(jtfuPKdqxspEwIScglJeCr({8820,11655,11655,11340})).Handle.Color = color
 		end
 	end)
 end
 
 function Night(option)
 getgenv().night = option
-if getgenv().night == eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({11550,11025,10815,10920,12180}) then
+if getgenv().night == jtfuPKdqxspEwIScglJeCr({11550,11025,10815,10920,12180}) then
 game.Lighting.ClockTime = 0
-elseif getgenv().night == eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({10500,10185,12705}) then
+elseif getgenv().night == jtfuPKdqxspEwIScglJeCr({10500,10185,12705}) then
 game.Lighting.ClockTime = 14
 end
 end
@@ -115,14 +117,14 @@ setreadonly(mt, false)
 local old = mt.__newindex
 
     mt.__newindex = newcclosure(function(a, b, c)
-    if tostring(a) == eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7560,12285,11445,10185,11550,11655,11025,10500}) and tostring(b) == eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({9135,10185,11340,11235,8715,11760,10605,10605,10500}) then
+    if tostring(a) == jtfuPKdqxspEwIScglJeCr({7560,12285,11445,10185,11550,11655,11025,10500}) and tostring(b) == jtfuPKdqxspEwIScglJeCr({9135,10185,11340,11235,8715,11760,10605,10605,10500}) then
        if getgenv().speed == true then
         return old(a, b, 1000)
        end
     end
     return old(a,b,c)
     end)
-  game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605})).Stepped:Connect(function()
+  game:GetService(jtfuPKdqxspEwIScglJeCr({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605})).Stepped:Connect(function()
        if getgenv().speed == true then
      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1000
       end
@@ -131,24 +133,24 @@ end
 
 function jump(v)
 getgenv().jump = v
-  game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8925,12075,10605,11970,7665,11550,11760,12285,12180,8715,10605,11970,12390,11025,10395,10605})).JumpRequest:Connect(function()
+  game:GetService(jtfuPKdqxspEwIScglJeCr({8925,12075,10605,11970,7665,11550,11760,12285,12180,8715,10605,11970,12390,11025,10395,10605})).JumpRequest:Connect(function()
 if getgenv().jump == true then
-     game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:FindFirstChildOfClass(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7560,12285,11445,10185,11550,11655,11025,10500})):ChangeState(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7770,12285,11445,11760,11025,11550,10815}))
+     game:GetService(jtfuPKdqxspEwIScglJeCr({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:FindFirstChildOfClass(jtfuPKdqxspEwIScglJeCr({7560,12285,11445,10185,11550,11655,11025,10500})):ChangeState(jtfuPKdqxspEwIScglJeCr({7770,12285,11445,11760,11025,11550,10815}))
 end
 end)
 end
 
 function rejoin()
-  game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605})):Teleport(game.PlaceId)
+  game:GetService(jtfuPKdqxspEwIScglJeCr({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605})):Teleport(game.PlaceId)
 end
 
 function infyield(v)
 getgenv().infyield = v
 if getgenv().infyield == true then
-  loadstring(game:HttpGet(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,7245,10500,10815,10605,7665,9345,4935,11025,11550,10710,11025,11550,11025,12180,10605,12705,11025,10605,11340,10500,4935,11445,10185,12075,12180,10605,11970,4935,12075,11655,12285,11970,10395,10605})))()
+  loadstring(game:HttpGet(jtfuPKdqxspEwIScglJeCr({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,7245,10500,10815,10605,7665,9345,4935,11025,11550,10710,11025,11550,11025,12180,10605,12705,11025,10605,11340,10500,4935,11445,10185,12075,12180,10605,11970,4935,12075,11655,12285,11970,10395,10605})))()
 elseif getgenv().infyield == false then
 for i,b in pairs(game.CoreGui:GetDescendants()) do
-if b.Name == eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7035,11445,10500,10290,10185,11970}) then
+if b.Name == jtfuPKdqxspEwIScglJeCr({7035,11445,10500,10290,10185,11970}) then
 b.Parent.Parent.Parent:Remove()
 end
 end
@@ -156,15 +158,15 @@ end
 end
 
 for i,v in pairs(workspace.Clouds:GetChildren()) do
-if v.Name == eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7035,11340,11655,12285,10500}) then
+if v.Name == jtfuPKdqxspEwIScglJeCr({7035,11340,11655,12285,10500}) then
 v.CanCollide = true
 end
 end
 
 function AntiAfk(v)
 getgenv().antiafk = v
-local bb=game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({9030,11025,11970,12180,12285,10185,11340,8925,12075,10605,11970}))
-game:GetService(eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Idled:Connect(function()
+local bb=game:GetService(jtfuPKdqxspEwIScglJeCr({9030,11025,11970,12180,12285,10185,11340,8925,12075,10605,11970}))
+game:GetService(jtfuPKdqxspEwIScglJeCr({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Idled:Connect(function()
 if getgenv().antiafk == true then
 bb:CaptureController()
 bb:ClickButton2(Vector2.new())
@@ -179,7 +181,7 @@ local mt = getrawmetatable(game)
         setreadonly(mt, false)
         mt.__namecall = protect(function(self, ...)
             local method = getnamecallmethod()
-            if method == eOCrjNwnhvLLjGrmvjzPXIxwbmTMlJQZlGTRuTXoeQiSpSreaRupYsACRzFB({7875,11025,10395,11235}) then
+            if method == jtfuPKdqxspEwIScglJeCr({7875,11025,10395,11235}) then
                 wait(9e9)
                 return
             end
