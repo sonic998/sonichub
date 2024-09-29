d085813aaf3fa46832cfa11c38c0b8bb0645d226
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-29 23:40:56 +0200

    Add files via upload

---
+++ Shit/HalloweenBpMap.lua
@@ -0,0 +1,9 @@
+for i,v in pairs(workspace:GetDescendants()) do
+if v.Name == "Baseplate" then
+if v.Color == Color3.fromRGB(91, 154, 76) then
+v.Color = Color3.fromRGB(229, 108, 43)
+end
+if v.Color == Color3.fromRGB(253, 234, 141) then
+v.Color = Color3.fromRGB(229, 108, 43)
+end
+end
