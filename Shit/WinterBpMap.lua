c51f729f49ae36838a9b242301783fd6bb16526e
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-29 17:40:10 +0200

    Create WinterBpMap.lua

---
+++ Shit/WinterBpMap.lua
@@ -0,0 +1,129 @@
+function czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
+for i,v in pairs(workspace:GetDescendants()) do
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({6930,10185,12075,10605,11760,11340,10185,12180,10605}) then
+if v.Color == Color3.fromRGB(91, 154, 76) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(253, 234, 141) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({8400,10185,11970,12180}) then
+if v.Color == Color3.fromRGB(91, 154, 76) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(255, 89, 89) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(190, 104, 98) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(159, 161, 172) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(17, 17, 17) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(253, 234, 141) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({8400,11025,11550,10605,3360,12180,11970,10605,10605,3360,11340,10605,10185,12390,10605,12075}) then
+if v.Color == Color3.fromRGB(44, 101, 29) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(245, 205, 48) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({9135,10605,10500,10815,10605}) then
+if v.Color == Color3.fromRGB(255, 89, 89) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(190, 104, 98) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(91, 154, 76) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(253, 234, 141) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({8715,10185,11550,10500,9135,10605,10500,10815,10605}) then
+if v.Color == Color3.fromRGB(253, 234, 141) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({8715,10185,11550,10500,7035,11655,11970,11550,10605,11970,9135,10605,10500,10815,10605}) then
+if v.Color == Color3.fromRGB(253, 234, 141) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({7035,11655,11970,11550,10605,11970,9135,10605,10500,10815,10605}) then
+if v.Color == Color3.fromRGB(255, 89, 89) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+if v.Color == Color3.fromRGB(190, 104, 98) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({8400,11025,11760,10605,8820,11655,11760}) then
+if v.Color == Color3.fromRGB(91, 93, 105) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({9135,10185,11340,11235,8400,10185,11970,12180}) then
+if v.Color == Color3.fromRGB(160, 95, 53) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({7455,11970,10185,12075,12075}) then
+if v.Color == Color3.fromRGB(31, 128, 29) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({8715,11235,12705,6825,11970,10605,10185,7350,11655,12285,11550,10500,10185,12180,11025,11655,11550}) then
+if v.Color == Color3.fromRGB(253, 234, 141) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+if v.Name == czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({6930,11970,11025,10500,10815,10605,7455,12285,10185,11970,10500}) then
+if v:IsA(czQlgnrtvgPntYZCHRcMDTqdgbcyWUfItlOWPIIvRIHudGKuuDfGraJJsimikveulIoAhvofInlJXaiFxaNyQeQUanrgKIcDaLGm({8400,10185,11970,12180})) then
+if v.Color == Color3.fromRGB(17, 17, 17) then
+v.Color = Color3.fromRGB(255, 255, 255)
+v.Material = Enum.Material.Snow
+end
+end
+end
+end
+for i,b in pairs(workspace.BoisBase:GetChildren()) do
+if b.Color == Color3.fromRGB(142, 75, 42) then
+b.Color = Color3.fromRGB(255, 255, 255)
+b.Material = Enum.Material.Snow
+end
+end    
