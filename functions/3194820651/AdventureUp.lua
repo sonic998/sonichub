9564d32900e1b7279bcc39aeb0083b90a6a122a3
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-30 22:21:56 +0200

    Update AdventureUp.lua

--- functions/3194820651/AdventureUp.lua
+++ functions/3194820651/AdventureUp.lua
@@ -1,4 +1,4 @@
-function xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+function oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
 
 
 function speed(v)
@@ -8,11 +8,12 @@ setreadonly(mt, false)
 local old = mt.__newindex
 
     mt.__newindex = newcclosure(function(a, b, c)
-    if tostring(a) == xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({7560,12285,11445,10185,11550,11655,11025,10500}) and tostring(b) == xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({9135,10185,11340,11235,8715,11760,10605,10605,10500}) then
+    if tostring(a) == oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({7560,12285,11445,10185,11550,11655,11025,10500}) and tostring(b) == oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({9135,10185,11340,11235,8715,11760,10605,10605,10500}) then
       if getgenv().speed == true then
         return old(a, b, 100)
         elseif getgenv().speed == false then
         return old(a, b, 32)
+    end
     end
     return old(a,b,c)
     end)
@@ -26,24 +27,24 @@ end
 
 function jump(v)
 getgenv().jump = v
-  game:GetService(xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({8925,12075,10605,11970,7665,11550,11760,12285,12180,8715,10605,11970,12390,11025,10395,10605})).JumpRequest:Connect(function()
+  game:GetService(oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({8925,12075,10605,11970,7665,11550,11760,12285,12180,8715,10605,11970,12390,11025,10395,10605})).JumpRequest:Connect(function()
   if getgenv().jump == true then
-     game:GetService(xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:FindFirstChildOfClass(xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({7560,12285,11445,10185,11550,11655,11025,10500})):ChangeState(xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({7770,12285,11445,11760,11025,11550,10815}))
+     game:GetService(oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:FindFirstChildOfClass(oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({7560,12285,11445,10185,11550,11655,11025,10500})):ChangeState(oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({7770,12285,11445,11760,11025,11550,10815}))
 end
 end)
 end
 
 function rejoin()
-  game:GetService(xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605})):Teleport(game.PlaceId)
+  game:GetService(oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605})):Teleport(game.PlaceId)
 end
 
 function infyield()
-  loadstring(game:HttpGet(xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,7245,10500,10815,10605,7665,9345,4935,11025,11550,10710,11025,11550,11025,12180,10605,12705,11025,10605,11340,10500,4935,11445,10185,12075,12180,10605,11970,4935,12075,11655,12285,11970,10395,10605})))()
+  loadstring(game:HttpGet(oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,7245,10500,10815,10605,7665,9345,4935,11025,11550,10710,11025,11550,11025,12180,10605,12705,11025,10605,11340,10500,4935,11445,10185,12075,12180,10605,11970,4935,12075,11655,12285,11970,10395,10605})))()
 end
 
 function AntiAfk()
-local bb=game:GetService(xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({9030,11025,11970,12180,12285,10185,11340,8925,12075,10605,11970}))
-game:GetService(xRryyoGGYhnMcvdKDchAwXEKKgKdZgWVwtgpketKFzemKfAKYKXsYkVNaDejmYrQTXwyF({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Idled:Connect(function()
+local bb=game:GetService(oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({9030,11025,11970,12180,12285,10185,11340,8925,12075,10605,11970}))
+game:GetService(oqDLKgVzaNakpTTWtnsecBdecwRQXJjKFtlxEqFuLwNn({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Idled:Connect(function()
 bb:CaptureController()
 bb:ClickButton2(Vector2.new())
 end)
