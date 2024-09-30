437754932bda1080d8d7b362d4c4c484ddb1e999
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-30 21:53:04 +0200

    Update HalloweenBpMap.lua

--- Shit/HalloweenBpMap.lua
+++ Shit/HalloweenBpMap.lua
@@ -1,9 +1,13 @@
+function FpdPdcLBjLMVNWaWqtHEsJKVxkVAGujhhSUvZMmfFSHHYAKTNxBTePjimWZwXrndVWeE(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
 for i,v in pairs(workspace:GetDescendants()) do
-if v.Name == "Baseplate" then
+if v.Name == FpdPdcLBjLMVNWaWqtHEsJKVxkVAGujhhSUvZMmfFSHHYAKTNxBTePjimWZwXrndVWeE({6930,10185,12075,10605,11760,11340,10185,12180,10605}) then
+v.Color = Color3.fromRGB(229, 108, 43)
+end
+if v.Name == FpdPdcLBjLMVNWaWqtHEsJKVxkVAGujhhSUvZMmfFSHHYAKTNxBTePjimWZwXrndVWeE({9135,10605,10500,10815,10605}) then
 if v.Color == Color3.fromRGB(91, 154, 76) then
 v.Color = Color3.fromRGB(229, 108, 43)
 end
-if v.Color == Color3.fromRGB(253, 234, 141) then
-v.Color = Color3.fromRGB(229, 108, 43)
 end
-end
+end  
