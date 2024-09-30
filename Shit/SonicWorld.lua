fd283f4e85904f2ff26fb5dae9fc9f4844315ad5
sonic <156506053+sonic998@users.noreply.github.com>
2024-09-30 16:57:11 +0200

    Create SonicWorld.lua

---
+++ Shit/SonicWorld.lua
@@ -0,0 +1,111 @@
+function jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
+local SonicWorld = Instance.new(jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({7350,11655,11340,10500,10605,11970}), workspace)
+SonicWorld.Name = jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({8715,11655,11550,11025,10395,3360,9135,11655,11970,11340,10500})
+
+local Palmtrees = Instance.new(jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({7350,11655,11340,10500,10605,11970}), SonicWorld)
+Palmtrees.Name = jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({8400,10185,11340,11445,3360,12180,11970,10605,10605,12075})
+
+local Baseplates = Instance.new(jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({7350,11655,11340,10500,10605,11970}), SonicWorld)
+Baseplates.Name = jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({6930,10185,12075,10605,11760,11340,10185,12180,10605,12075})
+
+function addPalmtree(pos)
+	local a = Instance.new(jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({8400,10185,11970,12180}), Palmtrees)
+	a.Reflectance = 0
+	a.Color = Color3.new(0.27451, 0.686275, 0.160784)
+	a.Anchored = true
+	a.RightSurface = Enum.SurfaceType.Smooth
+	a.Material = Enum.Material.Plastic
+	a.Size = Vector3.new(43.574153900146484, 15.797706604003906, 38.48719787597656)
+	a.BackSurface = Enum.SurfaceType.Smooth
+	a.BottomSurface = Enum.SurfaceType.Smooth
+	a.LeftSurface = Enum.SurfaceType.Smooth
+	a.Transparency = 0
+	a.Name = jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({10185})
+	a.CFrame = CFrame.new(-3764, -125.899956, -2074.00024, 0.999999404, 0, 0, 0, 1, 0, 0, 0, 1)
+	a.FrontSurface = Enum.SurfaceType.Smooth
+	a.TopSurface = Enum.SurfaceType.Smooth
+	local b = Instance.new(jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({8400,10185,11970,12180}), Palmtrees)
+	b.Reflectance = 0
+	b.Color = Color3.new(0.627451, 0.372549, 0.207843)
+	b.Anchored = true
+	b.RightSurface = Enum.SurfaceType.Smooth
+	b.Locked = false
+	b.Material = Enum.Material.Plastic
+	b.Archivable = true
+	b.Size = Vector3.new(7.002279281616211, 45.041934967041016, 12.273223876953125)
+	b.BackSurface = Enum.SurfaceType.Smooth
+	b.BottomSurface = Enum.SurfaceType.Smooth
+	b.CanCollide = true
+	b.LeftSurface = Enum.SurfaceType.Smooth
+	b.Transparency = 0
+	b.Name = jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({10290})
+	b.CFrame = CFrame.new(-3763.6958, -145.480133, -2069.16235, 0.999999285, 0, 0, 0, 1, 0, 0, 0, 0.999999881)
+	b.FrontSurface = Enum.SurfaceType.Smooth
+	b.TopSurface = Enum.SurfaceType.Smooth
+	local c = Instance.new(jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({8400,10185,11970,12180}), Palmtrees)
+	c.Reflectance = 0
+	c.Color = Color3.new(0.647059, 0.466667, 0.290196)
+	c.Anchored = true
+	c.RightSurface = Enum.SurfaceType.Smooth
+	c.Locked = false
+	c.Material = Enum.Material.Plastic
+	c.Archivable = true
+	c.Size = Vector3.new(5.212186813354492, 7.072818756103516, 5.496402740478516)
+	c.BackSurface = Enum.SurfaceType.Smooth
+	c.BottomSurface = Enum.SurfaceType.Smooth
+	c.CanCollide = true
+	c.LeftSurface = Enum.SurfaceType.Smooth
+	c.Transparency = 0
+	c.Name = jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({10395})
+	c.CFrame = CFrame.new(-3766.56348, -123.209061, -2070.43457, 0.999999404, 0, 0, 0, 1, 0, 0, 0, 1)
+	c.FrontSurface = Enum.SurfaceType.Smooth
+	c.TopSurface = Enum.SurfaceType.Smooth
+	local d = Instance.new(jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({8400,10185,11970,12180}), Palmtrees)
+	d.Reflectance = 0
+	d.Color = Color3.new(0.647059, 0.466667, 0.290196)
+	d.Anchored = true
+	d.RightSurface = Enum.SurfaceType.Smooth
+	d.Locked = false
+	d.Material = Enum.Material.Plastic
+	d.Archivable = true
+	d.Size = Vector3.new(5.212186813354492, 7.072818756103516, 5.496402740478516)
+	d.BackSurface = Enum.SurfaceType.Smooth
+	d.BottomSurface = Enum.SurfaceType.Smooth
+	d.CanCollide = true
+	d.LeftSurface = Enum.SurfaceType.Smooth
+	d.Transparency = 0
+	d.Name = jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({10500})
+	d.CFrame = CFrame.new(-3767.04565, -124.55645, -2079.11865, 0.999999404, 0, 0, 0, 1, 0, 0, 0, 1)
+	d.FrontSurface = Enum.SurfaceType.Smooth
+	d.TopSurface = Enum.SurfaceType.Smooth
+end
+
+function AddBaseplatePart(pos)
+	local part = Instance.new(jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({8400,10185,11970,12180}), Baseplates)
+	part.Reflectance = 0
+	part.Color = Color3.new(0.356863, 0.603922, 0.298039)
+	part.Anchored = true
+	part.RightSurface = Enum.SurfaceType.Smooth
+	part.Locked = false
+	part.Material = Enum.Material.Grass
+	part.Archivable = true
+	part.Size = Vector3.new(2047, 1, 2047)
+	part.BackSurface = Enum.SurfaceType.Smooth
+	part.BottomSurface = Enum.SurfaceType.Smooth
+	part.CanCollide = true
+	part.LeftSurface = Enum.SurfaceType.Smooth
+	part.Transparency = 0
+	part.Name = jFwmIGDDPBLFxJHepNweOlwvasQllIhHwdcBRmcOQJFCWAXqRvleCFywGkokSfHaoCkNNOjwIKkemuDpYOh({8400,10185,11970,12180})
+	part.CFrame = pos
+	part.FrontSurface = Enum.SurfaceType.Smooth
+	part.TopSurface = Enum.SurfaceType.Smooth
+end
+
+AddBaseplatePart(CFrame.new(-1906.5, -168.5, -2188.5))
+AddBaseplatePart(CFrame.new(-3927.5, -168.5, -2188.5))
+AddBaseplatePart(CFrame.new(-3909.5, -168.5, -4220.5))
+AddBaseplatePart(CFrame.new(-1883.5, -168.5, -4220.5))
+
+addPalmtree()    
