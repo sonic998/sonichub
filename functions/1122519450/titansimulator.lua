c4a944912352decca37a2e98a176308bf802de19
sonic <156506053+sonic998@users.noreply.github.com>
2024-10-01 20:52:13 +0200

    Update titansimulator.lua

--- functions/1122519450/titansimulator.lua
+++ functions/1122519450/titansimulator.lua
@@ -1,26 +1,31 @@
-function lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+function GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
 
 
-function speed()
+function speed(v)
+getgenv().speed = v
 local mt = getrawmetatable(game)
 setreadonly(mt, false)
 local old = mt.__newindex
 
     mt.__newindex = newcclosure(function(a, b, c)
-    if tostring(a) == lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({7560,12285,11445,10185,11550,11655,11025,10500}) and tostring(b) == lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({9135,10185,11340,11235,8715,11760,10605,10605,10500}) then
+    if tostring(a) == GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({7560,12285,11445,10185,11550,11655,11025,10500}) and tostring(b) == GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({9135,10185,11340,11235,8715,11760,10605,10605,10500}) then
+    if getgenv().speed == true then
         return old(a, b, 1000)
+    end
     end
     return old(a,b,c)
     end)
+    while getgenv().speed == true do wait()
      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1000
+end
 end
 
 function autoswing()
-game.Players.LocalPlayer.Character:WaitForChild(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({7140,10605,11445,11655,11550,3360,8715,12495,11655,11970,10500})):Activate()
+game.Players.LocalPlayer.Character:WaitForChild(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({7140,10605,11445,11655,11550,3360,8715,12495,11655,11970,10500})):Activate()
 end
 
 function autopunch()
-game.Players.LocalPlayer.Character:WaitForChild(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({8400,12285,11550,10395,10920})):Activate()
+game.Players.LocalPlayer.Character:WaitForChild(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({8400,12285,11550,10395,10920})):Activate()
 end
 
 function autorebirth()
@@ -28,30 +33,33 @@ game.ReplicatedStorage.RemoteEventContainer.Rebirth:FireServer()
 end
 
 function equippunch()
-game.Players.LocalPlayer.Character:WaitForChild(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({7560,12285,11445,10185,11550,11655,11025,10500})):EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({8400,12285,11550,10395,10920})))
+game.Players.LocalPlayer.Character:WaitForChild(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({7560,12285,11445,10185,11550,11655,11025,10500})):EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({8400,12285,11550,10395,10920})))
 end
 
 function equipsword()
-game.Players.LocalPlayer.Character:WaitForChild(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({7560,12285,11445,10185,11550,11655,11025,10500})):EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({7140,10605,11445,11655,11550,3360,8715,12495,11655,11970,10500})))
+game.Players.LocalPlayer.Character:WaitForChild(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({7560,12285,11445,10185,11550,11655,11025,10500})):EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({7140,10605,11445,11655,11550,3360,8715,12495,11655,11970,10500})))
 end
 
-function jump()
-game:GetService(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({8925,12075,10605,11970,7665,11550,11760,12285,12180,8715,10605,11970,12390,11025,10395,10605})).JumpRequest:Connect(function()
-     game:GetService(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:FindFirstChildOfClass(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({7560,12285,11445,10185,11550,11655,11025,10500})):ChangeState(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({7770,12285,11445,11760,11025,11550,10815}))
+function jump(v)
+getgenv().jump = v
+game:GetService(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({8925,12075,10605,11970,7665,11550,11760,12285,12180,8715,10605,11970,12390,11025,10395,10605})).JumpRequest:Connect(function()
+if getgenv().jump == true then
+     game:GetService(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Character:FindFirstChildOfClass(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({7560,12285,11445,10185,11550,11655,11025,10500})):ChangeState(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({7770,12285,11445,11760,11025,11550,10815}))
+end
 end)
 end
 
 function rejoin()
-game:GetService(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605})):Teleport(game.PlaceId)
+game:GetService(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({8820,10605,11340,10605,11760,11655,11970,12180,8715,10605,11970,12390,11025,10395,10605})):Teleport(game.PlaceId)
 end
 
 function infyield()
-  loadstring(game:HttpGet(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,7245,10500,10815,10605,7665,9345,4935,11025,11550,10710,11025,11550,11025,12180,10605,12705,11025,10605,11340,10500,4935,11445,10185,12075,12180,10605,11970,4935,12075,11655,12285,11970,10395,10605})))()
+  loadstring(game:HttpGet(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,7245,10500,10815,10605,7665,9345,4935,11025,11550,10710,11025,11550,11025,12180,10605,12705,11025,10605,11340,10500,4935,11445,10185,12075,12180,10605,11970,4935,12075,11655,12285,11970,10395,10605})))()
 end
 
 function AntiAfk()
-local bb=game:GetService(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({9030,11025,11970,12180,12285,10185,11340,8925,12075,10605,11970}))
-game:GetService(lSFzkRmjOgNUXaUuxFLwQxfOGdJGcIeQ({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Idled:Connect(function()
+local bb=game:GetService(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({9030,11025,11970,12180,12285,10185,11340,8925,12075,10605,11970}))
+game:GetService(GBbaRGoxLBRdKhjlpYykGgPWquJlrXhcNj({8400,11340,10185,12705,10605,11970,12075})).LocalPlayer.Idled:Connect(function()
 bb:CaptureController()
 bb:ClickButton2(Vector2.new())
 end)
