e22cec7b5613e7b933713b7ad8aa32fcd0d4727a
sonic <156506053+sonic998@users.noreply.github.com>
2024-10-05 12:18:29 +0200

    Update WeightLifting.lua

--- Scripts/WeightLifting.lua
+++ Scripts/WeightLifting.lua
@@ -47,6 +47,62 @@ end
 end)
 end)
 
+localplayer:Button("Fire", function()
+for i = 1,3 do
+local fire = Instance.new("Fire", game.Players.LocalPlayer.Character.HumanoidRootPart)
+fire.Heat = 25
+fire.size = 30
+local fire1 = Instance.new("Fire", game.Players.LocalPlayer.Character.Head)
+fire1.Heat = 25
+fire1.size = 30
+local fire2 = Instance.new("Fire", game.Players.LocalPlayer.Character.LeftFoot)
+fire2.Heat = 25
+fire2.size = 30
+local fire3 = Instance.new("Fire", game.Players.LocalPlayer.Character.LeftHand)
+fire3.Heat = 25
+fire3.size = 30
+local fire4 = Instance.new("Fire", game.Players.LocalPlayer.Character.LeftLowerArm)
+fire4.Heat = 25
+fire4.size = 30
+local fire5 = Instance.new("Fire", game.Players.LocalPlayer.Character.LeftLowerLeg)
+fire5.Heat = 25
+fire5.size = 30
+local fire6 = Instance.new("Fire", game.Players.LocalPlayer.Character.LeftUpperArm)
+fire6.Heat = 25
+fire6.size = 30
+local fire7 = Instance.new("Fire", game.Players.LocalPlayer.Character.LeftUpperLeg)
+fire7.Heat = 25
+fire7.size = 30
+local fire8 = Instance.new("Fire", game.Players.LocalPlayer.Character.LowerTorso)
+fire8.Heat = 25
+fire8.size = 30
+local fire9 = Instance.new("Fire", game.Players.LocalPlayer.Character.RightFoot)
+fire9.Heat = 25
+fire9.size = 30
+local fire10 = Instance.new("Fire", game.Players.LocalPlayer.Character.RightHand)
+fire10.Heat = 25
+fire10.size = 30
+local fire11 = Instance.new("Fire", game.Players.LocalPlayer.Character.RightLowerArm)
+fire11.Heat = 25
+fire11.size = 30
+local fire12 = Instance.new("Fire", game.Players.LocalPlayer.Character.RightLowerLeg)
+fire12.Heat = 25
+fire12.size = 30
+local fire13 = Instance.new("Fire", game.Players.LocalPlayer.Character.RightUpperArm)
+fire13.Heat = 25
+fire13.size = 30
+local fire14 = Instance.new("Fire", game.Players.LocalPlayer.Character.RightUpperLeg)
+fire14.Heat = 25
+fire14.size = 30
+local fire15 = Instance.new("Fire", game.Players.LocalPlayer.Character.UpperTorso)
+fire15.Heat = 25
+fire15.size = 30
+local fire16 = Instance.new("Fire", game.Players.LocalPlayer.Character.HumanoidRootPart)
+fire16.Heat = 25
+fire16.size = 30
+end
+end)
+
 misc:Button("Rejoin", function()
   game:GetService("TeleportService"):Teleport(game.PlaceId)
 end)
