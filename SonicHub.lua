82533731c7d889c232798e7f5252e79201eac2b1
sonic <156506053+sonic998@users.noreply.github.com>
2024-10-10 21:27:31 +0200

    Update SonicHub.lua

--- SonicHub.lua
+++ SonicHub.lua
@@ -7,7 +7,6 @@ local games = {
   ["ZombieAttack.lua"] = 1240123653,
   ["RabbitSimulator.lua"] = 1502601752,
   ["GorillaSimulator.lua"] = 1378712700,
-  ["UltraLiftingSimulator.lua"] = 13928941253,
   ["TreasureQuest.lua"] = 2960777560,
   ["AdventureUp.lua"] = 3194820651,
   ["EatingSimulator.lua"] = 1128235362,
