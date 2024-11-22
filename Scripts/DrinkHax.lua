ab6a3bdc60d5681a7b4d1045fbd6deaa3b1f698d
sonic <156506053+sonic998@users.noreply.github.com>
2024-11-22 09:11:02 -0800

    Update DrinkHax.lua

--- Scripts/DrinkHax.lua
+++ Scripts/DrinkHax.lua
@@ -385,7 +385,26 @@ bb:ClickButton2(Vector2.new())
 end)
 end)
 
-		
+		function WalkSpeed()
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+local old = mt.__newindex
+
+    mt.__newindex = newcclosure(function(a, b, c)
+    if tostring(a) == "Humanoid" and tostring(b) == "WalkSpeed" then
+       if getgenv().settings.walkspeed == true then
+        return old(a, b, 400)
+       end
+    end
+    return old(a,b,c)
+    end)
+  game:GetService("RunService").Stepped:Connect(function()
+       if getgenv().settings.walkspeed == true then
+     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 400
+      end
+  end)
+end
+		WalkSpeed()
 
 fastdrink:ChangeState(true)
 autodrink:ChangeState(true)
