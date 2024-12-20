ddca5b2db027505a7988ebacf52837f125337c8e
sonic <156506053+sonic998@users.noreply.github.com>
2024-12-20 20:56:35 +0100

    Create SonicSpeedSimulator.lua

---
+++ Scripts/SonicSpeedSimulator.lua
@@ -0,0 +1,25 @@
+local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
+
+local = win = lib:CreateWindow("GrannyXhub - Sonic Speed Simulator", "DarkLib")
+local autofarm = win:AddWindows("AutoFarm")
+local localplayer = win:AddWindows("LocalPlayer")
+local misc = win:AddWindows("Misc")
+local info = win:AddWindows("Info")
+
+localplayer:Button("Shift lock", function()
+loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Permanent-Shiftlock-7513"))()
+end)
+
+misc:Button("Rejoin", function()
+  game:GetService("TeleportService"):Teleport(game.PlaceId)
+end)
+
+misc:Toggle("Anti Afk", function()
+local bb=game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+bb:CaptureController()
+bb:ClickButton2(Vector2.new())
+end)
+end)
+
+info:Label("I do not support cheating in sonic games, I only allow bypassing the 20 minutes kick and some usefull tools and enabling shiftlock")
