705767c58d319e97665e787545b616b1252fb1d9
sonic <156506053+sonic998@users.noreply.github.com>
2024-12-31 12:01:43 +0100

    Update GorillaSimulator.lua

--- Scripts/GorillaSimulator.lua
+++ Scripts/GorillaSimulator.lua
@@ -1,6 +1,6 @@
 local lib = loadstring(game:HttpGet("https://sonic998.github.io/sonichub/UI.lua"))()
 
-_G.win = lib:CreateWindow("GrannyXhub - Gorilla Simulator 2", "MainUI")
+_G.win = lib:CreateWindow("Dedsec Scripts - Gorilla Simulator 2", "MainUI")
 local autofarm = _G.win:Page("AutoFarm")
 local localplayer = _G.win:Page("LocalPlayer")
 local teleport = _G.win:Page("Teleport")
